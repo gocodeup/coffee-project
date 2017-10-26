05c4f313881ed0ea27a5a6f40f08120536aa3252
floydmoore <floydimoore@yahoo.com>
2017-10-26 15:10:12 -0500

    coffee seperated

--- main.js
+++ main.js
@@ -1,35 +1,13 @@
-"use strict"
+"use strict";
 var coffeeName = document.getElementById("coffeename");
-coffeeName.addEventListener("keypress", function () {
+coffeeName.addEventListener("input", function () {
     var userinput = coffeeName.value;
     console.log(userinput);
     console.log("it is alive");
-    myFunction(userinput)
+    buildSearchHTML(userinput)
 
 
 });
-
-
-function myFunction(input) {
-    // Declare variables
-    var filter;
-    filter = input;
-    // Loop through all list items, and hide those who don't match the search query
-    for (var i = 0; i < coffees.length; i++) {
-        var  coffee = coffees[i].name;
-        if (coffee.indexOf(filter)> -1){
-            console.log(coffee);
-            var displayDiv = document.getElementById("coffeetypes");
-
-            displayDiv.innerHTML += coffee.toString() + "\n";
-
-
-        }
-
-
-    }
-}
-
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
@@ -46,6 +24,43 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'}
 ];
+
+function buildSearchHTML(searchInput) {
+
+    // Declare variables
+    var displayDiv = document.getElementById("coffeetypes");
+    displayDiv.innerHTML = "";
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (var i = 0; i < coffees.length; i++) {
+        var coffee = coffees[i];
+        var upperCoffee = coffee.name.toUpperCase();
+        var upperSearch = searchInput.toUpperCase();
+
+        if (upperCoffee.indexOf(upperSearch) > -1) {
+            console.log(coffee);
+
+            displayDiv.innerHTML += "<div>" + coffee.name + " " + coffee.roast + "</div>";
+
+    }
+}}
+
+// var coffees = [
+//     {id: 1, name: 'Light City', roast: 'light'},
+//     {id: 2, name: 'Half City', roast: 'light'},
+//     {id: 3, name: 'Cinnamon', roast: 'light'},
+//     {id: 4, name: 'City', roast: 'medium'},
+//     {id: 5, name: 'American', roast: 'medium'},
+//     {id: 6, name: 'Breakfast', roast: 'medium'},
+//     {id: 7, name: 'High', roast: 'dark'},
+//     {id: 8, name: 'Continental', roast: 'dark'},
+//     {id: 9, name: 'New Orleans', roast: 'dark'},
+//     {id: 10, name: 'European', roast: 'dark'},
+//     {id: 11, name: 'Espresso', roast: 'dark'},
+//     {id: 12, name: 'Viennese', roast: 'dark'},
+//     {id: 13, name: 'Italian', roast: 'dark'},
+//     {id: 14, name: 'French', roast: 'dark'}
+// ];
 
 
 
@@ -83,9 +98,12 @@ function updateCoffees(e) {
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
+            tbody.innerHTML = renderCoffees(filteredCoffees);
+        }
+        if (selectedRoast === "all"){
+            tbody.innerHTML = renderCoffees(coffees);
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
 
