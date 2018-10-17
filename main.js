132a06471d59b0df5ca473617c28b98f1449e08b
Ryan-Houpt <rehoupt@gmail.com>
2018-10-17 15:54:29 -0500

    project with working drop down and search bar

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html ='<div class="coffee col-6 float-left" id="myTable"'+ coffee.id+ '>';
+    var html ='<div class="coffee col-6 float-left" id="myInnerTable"'+ coffee.id+ '>';
     html +=  coffee.name +' '+ '<p>' + coffee.roast + '</p>';
     html += '</div>' ;
 
@@ -14,6 +14,20 @@ function renderCoffees(coffees) {
         html += renderCoffee(coffees[i]);
     }
     return html;
+}
+
+function updateCoffeesSearch(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = searchFilter.value;
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.name.toLowerCase().indexOf(selectedRoast) > -1) {
+            filteredCoffees.push(coffee);
+        } else if (selectedRoast === " "){
+            filteredCoffees = coffees
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
 function updateCoffees(e) {
@@ -45,16 +59,50 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
-
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
-var searchFilter = document.querySelector('#myInput')
+var searchFilter = document.querySelector('#myInput');
+var coffeeSearch = document.querySelector('#coffee');
+
+var coffeeObjectNames =
+    function getCoffeeNames() {
+    var i;
+    for (i = 0; i <= coffees.length; i++) {
+        var coffeeNames = coffees[i].name;
+        console.log(coffeeNames);
+    }
+};
+
+
 tbody.innerHTML = renderCoffees(coffees);
 
-
-
+// function searchBar (){
+//
+//     var m;
+//     for (m = 0; m < coffees.length; m++) {
+//         if (coffeeObjectNames.innerHTML.indexOf(searchFilter) > -1) {
+//             coffeeSearch[m].style.display = "";
+//         } else
+//             coffeeSearch[m].style.display = "none";
+//     }
+// }
 
 submitButton.addEventListener('click', updateCoffees);
 roastSelection.addEventListener('change', updateCoffees);
-// searchFilter.addEventListener('keypress', updateCoffees)
+searchFilter.oninput = updateCoffeesSearch;
+
+var addCoffeeRoast = document.querySelector('#AddRoast-selection').value;
+var addCoffeeName = document.querySelector('#addCoffeeName').value;
+var addCoffeeRoastArray = [addCoffeeRoast];
+var addCoffeeNameArray = [addCoffeeName];
+var addCoffeeSubmitButton = document.querySelector('#addCoffeeSubmit');
+
+function pushNewArray() {
+    coffees.push.roast(addCoffeeRoastArray);
+    coffees.push.name(addCoffeeNameArray);
+}
+
+addCoffeeSubmitButton.oninput = pushNewArray;
+
+console.log(pushNewArray());
