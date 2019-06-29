5ff50d4b6e60d96eb57eb4b0034d76fa7af7f8e5
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-06-28 21:30:31 -0500

    commit

--- main.js
+++ main.js
@@ -1,72 +1,5 @@
 "use strict";
 
-console.log("Owls can be powerful allies.");
-
-var searchInput = document.getElementById("input-coffee");
-//
-// searchInput.addEventListener('keydown', function(){
-//    console.log("Test");
-// });
-
-// var searchInput = document.getElementById("input-coffee");
-
-searchInput.addEventListener('keyup', function searchCoffees(){
-    const key = event.key.toLowerCase();
-    let siftedCoffees = [];
-    if (key === coffee.roast)
-    console.log(key);
-});
-
-function renderCoffee(coffee) {
-    var html = '<div class="coffee">';
-    // html += '<p>' + coffee.id + '</p>';
-    html += '<p>' + coffee.roast + ' - ' + coffee.name + '</p>';
-    html += '</div>';
-
-    return html;
-}
-
-function renderMultiCoffees(coffees) {
-    var html = '';
-    for (var i = coffees.length - 1; i >= 0; i--) {
-        html += renderCoffee(coffees[i]);
-    }
-    return html;
-}
-
-function updateCoffees(e) {
-    console.log("moo");
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value.toUpperCase();
-    var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        }
-    });
-    tbody.innerHTML = renderMultiCoffees(filteredCoffees);
-}
-
-
-
-// LIVE SEARCH FUNCTION //
-
-    function searchCoffees() {
-    console.log("moo!");
-        var searchRoast = document.getElementById('#input-coffee').value.toUpperCase();
-        var filteredCoffees = [];
-        console.log(searchRoast);
-        coffees.forEach(function(coffee) {
-            if (coffee.name.toUpperCase().includes(searchRoast)) {
-                filteredCoffees.push(coffee);
-                console.log(filteredCoffees);
-            }
-        });
-
-    tbody.innerHTML = renderMultiCoffees(filteredCoffees);
-}
-
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'LIGHT ROAST'},
     {id: 2, name: 'Half City', roast: 'LIGHT ROAST'},
@@ -88,7 +21,92 @@ var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
 
+var searchInput = document.getElementById("input-coffee");
+
+let siftedCoffees = [];
+
+// submitButton.addEventListener('click', updateCoffees);
+
+roastSelection.addEventListener('onchange', updateCoffees);
+
 tbody.innerHTML = renderMultiCoffees(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
-roastSelection.addEventListener('onchange', updateCoffees);
+
+searchInput.addEventListener('keydown', function(){
+   console.log("Test");
+});
+
+// var searchInput = document.getElementById("input-coffee");
+
+// searchInput.addEventListener('keyup', searchCoffees);
+//     const key = event.key.toLowerCase();
+//     console.log(key);
+//     siftedCoffees.push(key);
+//     console.log(siftedCoffees)
+//
+// });
+
+// LIVE SEARCH FUNCTION //
+
+    function searchCoffees() {
+        console.log("meow!");
+        var searchString = document.getElementById('input-coffee').value.toUpperCase();
+        var filteredCoffees = [];
+
+        filteredCoffees.forEach(function (coffee) {
+            if (coffee.name.toUpperCase().includes(searchString)) {
+                filteredCoffees.push(coffee);
+                console.log(filteredCoffees);
+            }
+        });
+    }
+
+    tbody.innerHTML = renderMultiCoffees(filteredCoffees);
+
+
+function renderCoffee(coffee) {
+    var html = '<div class="coffee">';
+    // html += '<p>' + coffee.id + '</p>';
+    html += '<p>' + coffee.roast + ' - ' + coffee.name + '</p>';
+    html += '</div>';
+
+    return html;
+}
+
+function renderMultiCoffees(coffees) {
+    var html = '';
+    for (var i = coffees.length - 1; i >= 0; i--) {
+        html += renderCoffee(coffees[i]);
+    }
+    return html;
+}
+
+// Drop-down menu roast update:
+function updateCoffees() {
+    console.log("moose bunnies");
+    // e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSelection.value.toUpperCase();
+    var filteredCoffees = [];
+
+    if (selectedRoast === 'ALL ROASTS') {
+        filteredCoffees = coffees;
+
+        } else {
+
+             coffees.forEach(function(coffee) {
+                if (coffee.roast === selectedRoast) {
+                    filteredCoffees.push(coffee);
+                } else {console.log("wookie!")};
+            });
+        }
+    tbody.innerHTML = renderMultiCoffees(filteredCoffees);
+}
+
+
+
+
+
+
+
+
+
