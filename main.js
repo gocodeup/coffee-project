270406a887f11d39e4dda9f032dcb7b6365cd5ff
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-08-20 18:17:33 -0500

    adjust js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function updateCoffees(e) {
     var filteredCoffees = [];
 
     coffees.forEach(function(coffee) {
-        if ( coffee.roast === selectedRoast && coffee.name.toLowerCase().includes(searchCoffee)) {
+        if ( coffee.roast === selectedRoast && coffee.name.toLowerCase().includes(searchCoffee.toLowerCase())) {
             filteredCoffees.push(coffee);
         }
     });
@@ -38,20 +38,20 @@ function updateCoffees(e) {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 14, name: 'French', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
-    {id: 1, name: 'Light City', roast: 'light'}
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
 ];
 var addCoffee = document.querySelector("#addCoffeeName");
 var coffeeSelector = document.querySelector("#roast-selection");
