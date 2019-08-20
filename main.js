e4128333a4c4cfaa0d76585e5644a7634e64027f
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-08-20 14:38:00 -0500

    add search function

--- main.js
+++ main.js
@@ -20,32 +20,38 @@ function renderCoffees(coffees) {
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
+    var searchCoffee = document.getElementById('addCoffeeName').value;
     var filteredCoffees = [];
+
     coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
+        if ( coffee.roast === selectedRoast && coffee.name.toLowerCase().includes(searchCoffee)) {
             filteredCoffees.push(coffee);
         }
     });
+
+
+    console.log(searchCoffee);
     var selector = document.getElementById("coffees");
     selector.innerHTML = renderCoffees(filteredCoffees);
 }
 
+
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 1, name: 'Light City', roast: 'light'},
 ];
 var addCoffee = document.querySelector("#addCoffeeName");
 var coffeeSelector = document.querySelector("#roast-selection");
@@ -59,4 +65,4 @@ submitButton.addEventListener('click', updateCoffees);
 
 coffeeSelector.addEventListener("change", updateCoffees);
 
-addCoffee.addEventListener("change", updateCoffees);
+addCoffee.addEventListener("keyup", updateCoffees);
