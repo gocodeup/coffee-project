2c175734b739c014986b4e624819de6b775679a0
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-08-22 16:42:16 -0500

    reversed coffees

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
-
+coffees.reverse();
 function createCoffee( name, roast) {
     coffees.unshift( {id: coffees.length + 1, name: name, roast: roast });
 }
