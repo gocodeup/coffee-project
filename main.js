f93d13f85604c3262e3545410aa3e20b24a439ed
William Stephens <william.a.stephens17@gmail.com>
2019-12-18 16:07:53 -0600

    Some bootstrap styling and capitalized roasts.

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function renderCoffees(coffees) {
 function updateCoffees() {
     var filteredCoffees = [];
     coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast || selectedRoast === "all") {
+        if (coffee.roast === selectedRoast || selectedRoast === "All") {
             if (coffee.name.toLowerCase().includes(selectedCoffee.toLowerCase())) {
                 filteredCoffees.push(coffee);
             }
@@ -39,26 +39,26 @@ function updateCoffees() {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'}
+    {id: 1, name: 'Light City', roast: 'Light'},
+    {id: 2, name: 'Half City', roast: 'Light'},
+    {id: 3, name: 'Cinnamon', roast: 'Light'},
+    {id: 4, name: 'City', roast: 'Medium'},
+    {id: 5, name: 'American', roast: 'Medium'},
+    {id: 6, name: 'Breakfast', roast: 'Medium'},
+    {id: 7, name: 'High', roast: 'Dark'},
+    {id: 8, name: 'Continental', roast: 'Dark'},
+    {id: 9, name: 'New Orleans', roast: 'Dark'},
+    {id: 10, name: 'European', roast: 'Dark'},
+    {id: 11, name: 'Espresso', roast: 'Dark'},
+    {id: 12, name: 'Viennese', roast: 'Dark'},
+    {id: 13, name: 'Italian', roast: 'Dark'},
+    {id: 14, name: 'French', roast: 'Dark'}
 ];
 
 var coffeeContainer = document.getElementById('coffee-container');
 var submitButton = document.querySelector('#submit');
 
-var selectedRoast = 'all';
+var selectedRoast = 'All';
 var roastSelection = document.querySelector('#roast-selection');
 roastSelection.addEventListener('change', function(){
     selectedRoast = roastSelection.value;
