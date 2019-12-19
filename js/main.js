a71ef63da67f31ea3de4875731990c26aabad880
faithgarza <faith.garza001@gmail.com>
2019-12-19 11:13:34 -0600

    progress

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
-"use strict"
-
-function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
-
-    return html;
+"use strict";
+​
+function renderCoffee(coffee) {//sets up table//
+    var html = '<div class="coffee">';
+    html += '<h1>' + coffee.name + '</h1>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
+​
+    return html
 }
-
-function renderCoffees(coffees) {
+​
+function renderCoffees(filteredList) {
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
-        html += renderCoffee(coffees[i]);
+    for (var i = filteredList.length - 1; i >= 0; i--) {
+        html += renderCoffee(filteredList[i]);
     }
     return html;
 }
-
+​
+​
+​
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
@@ -27,48 +28,33 @@ function updateCoffees(e) {
             filteredCoffees.push(coffee);
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+    // tbody.innerHTML = renderCoffees(filteredCoffees);
+    coffeeList.innerHTML = renderCoffees(filteredCoffees);
 }
-
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-//This is your search bar do not delete
-
-
-
-function myFunction() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+​
+function searchCoffees(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var searchInput = userInput.value.toLowerCase();
+    var filteredCoffees = [];
+    coffees.forEach(function (coffee) {
+        var currentRoast = coffee.roast.toLowerCase();
+        var currentName = coffee.name.toLowerCase();
+        if (currentRoast.includes(searchInput) || currentName.includes(searchInput)) {
+            filteredCoffees.push(coffee);
         }
-    }
+    });
+    coffeeList.innerHTML = renderCoffees(filteredCoffees);
 }
-
-
-
-
-
-
-
+​
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
     {id: 3, name: 'Cinnamon', roast: 'light'},
     {id: 4, name: 'City', roast: 'medium'},
     {id: 5, name: 'American', roast: 'medium'},
     {id: 6, name: 'Breakfast', roast: 'medium'},
     {id: 7, name: 'High', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light'},
     {id: 8, name: 'Continental', roast: 'dark'},
     {id: 9, name: 'New Orleans', roast: 'dark'},
     {id: 10, name: 'European', roast: 'dark'},
@@ -77,11 +63,17 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
-
-var tbody = document.querySelector('#coffees');
+​
+​
+var coffeeList = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
-
-tbody.innerHTML = renderCoffees(coffees);
-
+var userInput = document.querySelector('#search-bar');
+var searchSubmit = document.querySelector('#search-btn');
+​
+​
+coffeeList.innerHTML = renderCoffees(coffees.sort(function(a, b){return a.id-b.id}));
+​
 submitButton.addEventListener('click', updateCoffees);
+searchSubmit.addEventListener("click", searchCoffees);
+userInput.addEventListener("keyup", searchCoffees);
