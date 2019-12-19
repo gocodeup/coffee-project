c35be9aae8d175526784e1af7d03ffdb68f57a7b
faithgarza <faith.garza001@gmail.com>
2019-12-19 12:11:59 -0600

    progress refactor

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 "use strict";
-​
+
 function renderCoffee(coffee) {//sets up table//
     var html = '<div class="coffee">';
     html += '<h1>' + coffee.name + '</h1>';
     html += '<p>' + coffee.roast + '</p>';
     html += '</div>';
-​
+
     return html
 }
-​
+
 function renderCoffees(filteredList) {
     var html = '';
     for (var i = filteredList.length - 1; i >= 0; i--) {
@@ -16,9 +16,9 @@ function renderCoffees(filteredList) {
     }
     return html;
 }
-​
-​
-​
+
+
+
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
@@ -31,7 +31,7 @@ function updateCoffees(e) {
     // tbody.innerHTML = renderCoffees(filteredCoffees);
     coffeeList.innerHTML = renderCoffees(filteredCoffees);
 }
-​
+
 function searchCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var searchInput = userInput.value.toLowerCase();
@@ -45,7 +45,7 @@ function searchCoffees(e) {
     });
     coffeeList.innerHTML = renderCoffees(filteredCoffees);
 }
-​
+
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 2, name: 'Half City', roast: 'light'},
@@ -63,17 +63,17 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
-​
-​
+
+
 var coffeeList = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
 var userInput = document.querySelector('#search-bar');
 var searchSubmit = document.querySelector('#search-btn');
-​
-​
+
+
 coffeeList.innerHTML = renderCoffees(coffees.sort(function(a, b){return a.id-b.id}));
-​
+
 submitButton.addEventListener('click', updateCoffees);
 searchSubmit.addEventListener("click", searchCoffees);
 userInput.addEventListener("keyup", searchCoffees);
