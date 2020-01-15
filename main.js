b40b570a81352b2841f2dd29c6a0bbf5378923e6
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-15 15:21:00 -0600

    added new features

--- main.js
+++ main.js
@@ -1,84 +1,122 @@
-"use strict"
+"use strict";
 
-function renderCoffee(coffee) {
-    var html = '<div class="coffee">';
-    // html += '<p>' + coffee.id +
-    html += '<h1>' + coffee.name + '</h1>';
-    html += '<p>' + coffee.roast + '</p>';
-    html += '</div>';
-
-    return html;
-}
+var coffeeList = document.getElementById('coffees');
 
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
-        html += renderCoffee(coffees[i]);
+    coffees.forEach(function (coffee) {
+        html += renderCoffee(coffee);
+    });
+    return html;
+}
+
+function updateCoffees() {
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if(coffee.roast === selectedRoast || selectedRoast === 'all roast') {
+            if(coffee.name.toLowerCase().includes(selectCoffee.toLowerCase())) {
+                filteredCoffees.push(coffee);
+            }
+        }
+    });
+    coffeeList.innerHTML = renderCoffees(filteredCoffees)
+}
+
+function addCoffee(inputName, roastType, inputRating) {
+    if(roastType !== "Roasts" && inputRating !== "Nicolas Rating") {
+        var addNewCoffee = {id: coffees.length + 1, name: inputName, roast: roastType, rating: inputRating};
+        coffees.push(addNewCoffee);
+        localStorage.setItem("coffees", JSON.stringify(coffees));
+    }
+
+}
+
+function removeCoffees(coffeeInput) {
+    coffees.forEach(function(coffee) {
+        if(coffee.name.toLowerCase() === coffeeInput.toLowerCase()) {
+            coffees.splice(coffees.indexOf(coffee), 1);
+        }
+        localStorage.setItem("coffees", JSON.stringify(coffees));
+    });
+}
+
+function makeNicolas(inputCoffee) {
+    var html = '';
+    for(var i = 0; i < inputCoffee.rating; i++) {
+        html += '<img class="rotation" src="img/nic-icon2.png" alt="nic-icon.png">'
     }
     return html;
 }
 
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-           filteredCoffees.push(coffee);
-        } else if(coffee.allRoast === selectedRoast)
-           filteredCoffees.push(coffee);
-    });
-    pList.innerHTML = renderCoffees(filteredCoffees);
+function renderCoffee(coffee) {
+    var html = '<div class="coffee">';
+    html += '<h3>' + coffee.name + ' ' + makeNicolas(coffee) + '</h3>' + '<p>' + coffee.roast + '</p>';
+    html += '</div>';
+    return html;
 }
 
-function search_coffee() {
-    let input = document.getElementById('searchbar').value;
-    input=input.toLowerCase();
-    let x = document.getElementsByClassName('coffee');
-    for (var i = 0; i < x.length; i++) {
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="list-item";
-        }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'National Treasure 2 Light City', roast: 'light', allRoast: "all roast" },
-    {id: 2, name: 'National Treasure Half City', roast: 'light', allRoast: "all roast" },
-    {id: 3, name: 'Con-Air Cinnamon', roast: 'light', allRoast: "all roast" },
-    {id: 4, name: 'Drive Angry City', roast: 'medium', allRoast: "all roast" },
-    {id: 5, name: 'Gone in Sixty Seconds American', roast: 'medium', allRoast: "all roast" },
-    {id: 6, name: 'Face-Off Breakfast', roast: 'medium', allRoast: "all roast" },
-    {id: 7, name: 'Matchstick Men High', roast: 'dark', allRoast: "all roast" },
-    {id: 8, name: 'The Wicker Man Continental', roast: 'dark', allRoast: "all roast" },
-    {id: 9, name: 'Lord Of War New Orleans', roast: 'dark', allRoast: "all roast" },
-    {id: 10, name: 'The European Ant Bully', roast: 'dark', allRoast: "all roast" },
-    {id: 11, name: 'World Trade Center Espresso', roast: 'dark', allRoast: "all roast" },
-    {id: 12, name: 'Bangkok Dangerous Viennese', roast: 'dark', allRoast: "all roast" },
-    {id: 13, name: 'Grindhouse Italian', roast: 'dark', allRoast: "all roast" },
-    {id: 14, name: 'The French Family Man', roast: 'dark', allRoast: "all roast"},
+    {id: 1, name: 'National Treasure 2 Light City', roast: 'light', rating: 5},
+    {id: 2, name: 'National Treasure Half City', roast: 'light', rating: 3},
+    {id: 3, name: 'Con-Air Cinnamon', roast: 'light', rating: 5},
+    {id: 4, name: 'Drive Angry City', roast: 'medium', rating: 2},
+    {id: 5, name: 'Gone in Sixty Seconds American', roast: 'medium', rating: 4},
+    {id: 6, name: 'Face-Off Breakfast', roast: 'medium', rating: 5},
+    {id: 7, name: 'Matchstick Men High', roast: 'dark', rating: 5},
+    {id: 8, name: 'The Wicker Man Continental', roast: 'dark', rating: 1},
+    {id: 9, name: 'Lord Of War New Orleans', roast: 'dark', rating: 3},
+    {id: 10, name: 'The European Ant Bully', roast: 'dark', rating: 4},
+    {id: 11, name: 'World Trade Center Espresso', roast: 'dark', rating: 5},
+    {id: 12, name: 'Bangkok Dangerous Viennese', roast: 'dark', rating: 2},
+    {id: 13, name: 'Grindhouse Italian', roast: 'dark', rating: 3},
+    {id: 14, name: 'The French Family Man', roast: 'dark', rating: 2}
 ];
 
-
-var pList = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
+var selectedRoast = 'all roast';
 var roastSelection = document.querySelector('#roast-selection');
+roastSelection.addEventListener('change', function() {
+    selectedRoast = roastSelection.value;
+    updateCoffees()
+});
+
+var selectCoffee = '';
+var searchCoffee = document.getElementById('searchbar');
+searchCoffee.addEventListener('keyup', function() {
+    selectCoffee = searchCoffee.value;
+    updateCoffees();
+});
 
 
-// tbody.innerHTML = renderCoffees(coffees);
+var addRoast = document.querySelector('#button');
+addRoast.addEventListener('click', function() {
+    var coffeeName = document.getElementById('addCoffees');
+    var coffeeRoast = document.getElementById('roast-selection2');
+    var coffeeRating = document.getElementById('nicRating');
+    if(coffeeName.value !== '') {
+        addCoffee(coffeeName.value, coffeeRoast.value, coffeeRating.value);
+        updateCoffees();
+    }
+    coffeeName.value = '';
+});
 
-roastSelection.addEventListener('click', updateCoffees);
-submitButton.addEventListener('click', updateCoffees);
+var removeCoffee = document.querySelector('#button2');
+removeCoffee.addEventListener('click', function() {
+    var removeCoffeeInput = document.getElementById('removeCoffees');
+    if(removeCoffeeInput.value !== '') {
+        removeCoffees(removeCoffeeInput.value);
+        updateCoffees()
+    }
+    removeCoffeeInput.value = '';
+});
+
+
+
+
+if(localStorage.getItem("coffees") !== null) {
+    coffees = localStorage.getItem("coffees");
+    coffees = JSON.parse(coffees);
+    coffeeList.innerHTML = renderCoffees(coffees);
+} else {
+    coffeeList.innerHTML = renderCoffees(coffees);
+
+}
