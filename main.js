b16a494314c4f50e3212409551e0b4dba083a473
Andrew Ball <andy.bob.ball@gmail.com>
2020-02-21 11:15:10 -0600

    push

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-"use strict"
+"use strict";
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+    var html = '<div class="coffee">';
+    html += '<p>' + coffee.id + '</p>';
+    html += '<h4>' + coffee.name + '</h4>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
 
     return html;
 }
