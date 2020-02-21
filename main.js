4d974966afac0adf4ce621225cdbfd1e5b4a7ac6
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-02-21 15:02:12 -0600

    Added js functionality

--- main.js
+++ main.js
@@ -1,34 +1,60 @@
 "use strict"
 
-function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+function renderCoffee(coffee) { //put the coffees into a table
+    var html = '<div class="col-4 coffee" id="big-div">'; //define class and table row
+    html += '<div class="coffee-data"><a href="#">' + coffee.id + '</a></div>'; // get id
+    html += '<div class="coffee-data"><a href="#">' + coffee.name + '</a></div>'; // get name
+    html += '<div class="coffee-data"><a href="#">' + coffee.roast + '</a></div>'; // ger roast
+    html += '</div>'; // end table row
 
-    return html;
+    return html; //return the table to renderCoffee
 }
 
-function renderCoffees(coffees) {
+function renderCoffees(coffees) { //loops through coffees, add each coffee to html
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    for(var i = 0; i <= coffees.length - 1; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
 
+
+//coffee roast selection
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
+
+    var selectedRoast = roastSelection.value; //filter the selection
+    var filteredCoffees = []; // the bucket
+
+    if(selectedRoast !== 'all') { //not all? do this
+        coffees.forEach(function(coffee) { //iterate through entire object
+            if (coffee.roast === selectedRoast) { // does the roast selection match? do this
+                filteredCoffees.push(coffee); // push the item to bucket
+            }
+        });
+        tbody.innerHTML = renderCoffees(filteredCoffees); //display the bucket
+    } else { // do it match the all? do this
+        tbody.innerHTML = renderCoffees(coffees); //show all coffee items
+    }
+
+}
+
+
+function updateInputCoffees(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+
+    var selectedRoast = inputSelection.value.toLowerCase();// standardize input selection
+    var filteredCoffees = []; //The bucket
+
+    coffees.forEach(function(coffee) { //
+        if(coffee.name.toLowerCase().match(selectedRoast) || coffee.roast.toLowerCase().match(selectedRoast)) {
             filteredCoffees.push(coffee);
         }
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
+
+
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
@@ -49,9 +75,12 @@ var coffees = [
 ];
 
 var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+var submitButton = document.getElementById('roast-selection');
+var roastSelection = document.querySelector('#roast-selection'); //user input coffee selection
+var inputSelection = document.getElementById('inputSelection');
 
-tbody.innerHTML = renderCoffees(coffees);
+tbody.innerHTML = renderCoffees(coffees); //adds coffees to table body
 
-submitButton.addEventListener('click', updateCoffees);
+submitButton.addEventListener('change', updateCoffees);
+inputSelection.addEventListener('keyup', updateInputCoffees);
+
