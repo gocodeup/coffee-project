11b181da550ef675110d10afc74b7cd77a832b72
Andrew Ball <andy.bob.ball@gmail.com>
2020-02-21 16:14:09 -0600

    push

--- main.js
+++ main.js
@@ -70,13 +70,13 @@ function myFunction() {
 // coffee list
 var tbody = document.querySelector('#coffees');
 
-var submitButton = document.querySelector('#submit');
+// var submitButton = document.querySelector('#submit');
 
 var roastSelection = document.querySelector('#roast-selection');
 
 tbody.innerHTML = renderCoffees(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
+roastSelection.addEventListener('click', updateCoffees);
 
 var textInput = document.querySelector('#myInput');
 
