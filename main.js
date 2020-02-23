aebbe07df35b5d28743b90a75443c29ea481530c
Dani Marie <mariedwilliams2@gmail.com>
2020-02-22 22:32:46 -0600

    Cofee-Project

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 "use strict";
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+    var html = '<div class="coffee">';
+    html += '<h3>' + coffee.name + '</h3>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
 
     return html;
 }
 
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = 0; i < coffees.length;  i++) {
+    for (var i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
+
     return html;
 }
-
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
@@ -28,6 +28,9 @@ function updateCoffees(e) {
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
+
+// tbody.innerHTML = renderCoffeeList(filterByRoast(coffees, roastSelection.value));
+// }
 
 
 var search = function updateCoffeesTwo(e) {
@@ -42,7 +45,24 @@ var search = function updateCoffeesTwo(e) {
     tbody.innerHTML = renderCoffees(filteredCoffeesTwo);
 };
 
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+// function updateCoffeesTwo(e) {
+//     e.preventDefault(); // don't submit the form, we just want to update the data
+//     var nameSelection = document.querySelector('#searchbar');
+// ​
+//     tbody.innerHTML = renderCoffeeList(filterByName(coffees, nameSelection.value));
+// }
+// ​
+// function filterByRoast(coffeeList, value){
+//     return coffeeList.filter( coffee => coffee.roast === value)
+// }
+// ​
+// function filterByName(coffeeList, value){
+//     return coffeeList.filter( coffee => coffee.name.toLowerCase() === value.toLowerCase())
+// }
+
+
+
+///Coffee
 var coffees = [
     {id: 1, name: 'The Greenbelt', roast: 'medium'},
     {id: 2, name: 'Discovery Green', roast: 'medium'},
@@ -59,6 +79,18 @@ var coffees = [
     {id: 13, name: 'Italiano', roast: 'dark'},
     {id: 14, name: 'Francais', roast: 'dark'},
 ];
+//
+// tbody.innerHTML = renderCoffeeList(coffees);
+// ​
+// submitTwoButton.addEventListener('click', updateCoffeesTwo);
+// submitButton.addEventListener('click', updateCoffees);
+// ​
+// document.getElementById('searchbar').addEventListener('keyup', function(event) {
+//     var searchString = event.target.value.toLowerCase();
+// ​
+//     tbody.innerHTML = renderCoffeeList(filterResults(coffees, searchString));
+// });
+
 
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
@@ -115,4 +147,4 @@ document.getElementById('searchbar').addEventListener('keydown', function(event)
         });
     }
 
-});
+})
