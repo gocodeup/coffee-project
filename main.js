446ec0ba2aea1820ac0f2fdfe91790464ddddf95
Anthony San Miguel <asanmiguel1088@gmail.com>
2020-02-25 16:02:20 -0600

    added changes

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
-
+coffees.reverse();
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var searchBar = document.querySelector('#search');
