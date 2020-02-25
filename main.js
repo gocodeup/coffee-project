080c16ce2174346cf9ba110c9dbbb30f04394e47
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-02-24 21:53:02 -0600

    Adjusted card html and css for hover effect

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ function renderCoffee(coffee) { //put the coffees into a table
     var html = '<div class="card coffee card-body d-flex justify-content-center text-center" id="big-div">'; //define class and table row
     // html += '<div class="coffee-data">' + coffee.id + '</a></div>'; // get id
     html += '<div class="coffee-data card-img-top">' + coffee.img + '</div>';
-    html += '<div class="coffee-data card-title"> ' + coffee.name + '</div>'; // get name
-    html += '<div class="coffee-data card-text" >' + coffee.roast + '</div>'; // ger roast
+    html += '<div class="coffee-data card-title" > ' + coffee.name + '</div>'; // get name
+    html += '<div class="coffee-data card-region">' + coffee.region + '</div>'; // ger sourced region
+    html += '<div class="coffee-data card-roast">' + coffee.roast + '</div>'; // ger roast
+    html += '<div class="coffee-data card-price" >' + coffee.price + '</div>'; // ger price
     html += '</div>'; // end table row
 
     return html; //return the table to renderCoffee
@@ -50,31 +52,32 @@ function updateInputCoffees(e) {
     coffees.forEach(function(coffee) { //
         if(coffee.name.toLowerCase().match(selectedRoast) || coffee.roast.toLowerCase().match(selectedRoast)) {
             filteredCoffees.push(coffee);
-        }else{
-            document.getElementById("error-message").innerHTML = "<img src='img/sad.png'>" + "Whoops, we could not find your roast"; //show error message
+        } else if (coffees.roast === " "){
+            tbody.innerHTML = renderCoffees(coffees); //show all coffee items
+        }else {
+            document.getElementById("error-message").innerHTML =  "Whoops, we could not find your roast"; //show error message
         }
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
 
-
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light', img:'<img src="img/PngItem_3904454.png" id="image" >'},
-    {id: 2, name: 'Half City', roast: 'light', img:'<img src="img/PngItem_3904454.png" id="image">'},
-    {id: 3, name: 'Cinnamon', roast: 'light', img:'<img src="img/PngItem_3904454.png" id="image">'},
-    {id: 4, name: 'City', roast: 'medium', img:'<img src="img/PngItem_3904454.png" id="image">'},
-    {id: 5, name: 'American', roast: 'medium', img:'<img src="img/PngItem_3904454.png" id="image">'},
-    {id: 6, name: 'Breakfast', roast: 'medium', img:'<img src="img/PngItem_3904454.png" id="image">'},
-    {id: 7, name: 'High', roast: 'dark', img:'<img src="img/PngItem_3904454.png" id="image">'},
-    {id: 8, name: 'Continental', roast: 'dark', img:'<img src="img/PngItem_3904454.png" id="image">'},
-    {id: 9, name: 'New Orleans', roast: 'dark', img:'<img src="img/PngItem_3904454.png" id="image">'},
-    {id: 10, name: 'European', roast: 'dark', img:'<img src="img/PngItem_3904454.png" id="image">'},
-    {id: 11, name: 'Espresso', roast: 'dark', img:'<img src="img/PngItem_3904454.png" id="image">'},
-    {id: 12, name: 'Viennese', roast: 'dark', img:'<img src="img/PngItem_3904454.png" id="image">'},
-    {id: 13, name: 'Italian', roast: 'dark', img:'<img src="img/PngItem_3904454.png" id="image">'},
-    {id: 14, name: 'French', roast: 'dark', img:'<img src="img/PngItem_3904454.png" id="image">'},
+    {id: 1, name: 'Light City', region: "Columbia", roast: 'light', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image" >'},
+    {id: 2, name: 'Half City', region: "Columbia", roast: 'light', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image">'},
+    {id: 3, name: 'Cinnamon', region: "Brazil", roast: 'light', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image">'},
+    {id: 4, name: 'City', region: "Columbia", roast: 'medium', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image">'},
+    {id: 5, name: 'American', region: "Ethiopia", roast: 'medium', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image">'},
+    {id: 6, name: 'Breakfast', region: "Columbia", roast: 'medium', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image">'},
+    {id: 7, name: 'High', region: "Columbia", roast: 'dark', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image">'},
+    {id: 8, name: 'Continental', region: "Columbia", roast: 'dark', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image">'},
+    {id: 9, name: 'New Orleans', region: "Ethiopia", roast: 'dark', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image">'},
+    {id: 10, name: 'European', region: "Brazil", roast: 'dark', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image">'},
+    {id: 11, name: 'Espresso', region: "Columbia", roast: 'dark', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image">'},
+    {id: 12, name: 'Viennese', region: "Brazil", roast: 'dark', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image">'},
+    {id: 13, name: 'Italian', region: "Ethiopia", roast: 'dark', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image">'},
+    {id: 14, name: 'French', region: "Brazil", roast: 'dark', price: "$14.75", img:'<img src="img/PngItem_3904454.png" id="image">'},
 ];
 
 var tbody = document.querySelector('#coffees');
