d33620522e4cf38546fb7a44d3467a5bbac1fba2
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-02-24 20:29:05 -0600

    added new code to add coffee

--- main-test.js
+++ main-test.js
@@ -59,23 +59,46 @@ function chooseCoffee() {
     }
 }
 
+//this function added to add/create new coffee added by user
+function newCoffee() {
+    var newCoffeeObject = {};
+    newCoffeeObject.name = newCoffeeName.value;
+    newCoffeeObject.roast = newCoffeeRoast.value;
+    newCoffeeObject.id = coffees.length + 1;
+    coffees.push(newCoffeeObject);
+    localStorage.setItem('locallyStoredCoffee', JSON.stringify(coffees));
+    body.innerHTML = renderCoffees(coffees);
+    return selectionOrder.innerHTML = renderCoffee(coffees);
+}
+
+var newCoffeeName = document.querySelector('#new-coffee'); //var added to be used in function newCoffee
+var newCoffeeRoast = document.querySelector('#new-roast'); //var added to be used in function newCoffee
+
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
+var coffees; //declaring coffees var to be used in if statement below which is used to determine if new coffee is being added.
+
+if (localStorage.getItem('locallyStoredCoffee') === null) {
+    coffees = [
+        {id: 1, name: 'Light City', roast: 'light'},
+        {id: 2, name: 'Half City', roast: 'light'},
+        {id: 3, name: 'Cinnamon', roast: 'light'},
+        {id: 4, name: 'City', roast: 'medium'},
+        {id: 5, name: 'American', roast: 'medium'},
+        {id: 6, name: 'Breakfast', roast: 'medium'},
+        {id: 7, name: 'High', roast: 'dark'},
+        {id: 8, name: 'Continental', roast: 'dark'},
+        {id: 9, name: 'New Orleans', roast: 'dark'},
+        {id: 10, name: 'European', roast: 'dark'},
+        {id: 11, name: 'Espresso', roast: 'dark'},
+        {id: 12, name: 'Viennese', roast: 'dark'},
+        {id: 13, name: 'Italian', roast: 'dark'},
+        {id: 14, name: 'French', roast: 'dark'},
+    ];
+    //else statement below added to add new coffee to local storage
+} else {
+    coffees = JSON.parse(localStorage.getItem('locallyStoredCoffee'))
+}
+localStorage.setItem('locallyStoredCoffee', JSON.stringify(coffees));
 
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
@@ -83,6 +106,8 @@ var roastSelection = document.querySelector('#roast-selection');
 
 //this auto selects the coffees that show up on page as user types letter at a time
 document.getElementById("coffee-input").addEventListener("keyup", chooseCoffee);
+
+document.getElementById("submitNewCoffee").addEventListener("click", newCoffee);
 
 tbody.innerHTML = renderCoffees(coffees);
 
