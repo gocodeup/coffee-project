8da292b848d42c167d6988d8348a81431df45506
James Spicer <spicer.r.james@gmail.com>
2020-02-25 23:10:58 -0600

    trouble with images being cut off

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 "use strict";
 
 function renderCoffee(coffee) {
-    var html = '<h1 class="coffee">';
+    var html = '<div id="rendered_coffee" style="border: 12px solid lightslategray; border-radius: 15px; margin-bottom: 3em; background-color: linen">' + '<h1 class="coffee">';
     html += '<div class="hidden">' + coffee.id + '</div>';
-    html += '<h1>' + coffee.name + '</h1>';
-    html += '<p>' + coffee.roast + '</p>';
-    html += '</h1>';
-
+    html += '<p>' + coffee.img + '</p>';
+    html += '<h1><em>' + coffee.name + '</em></h1>';
+    html += '<p>' + 'Roast Type: ' + coffee.roast + '</p>';
+    html += '</h1>' + '</div>';
     return html;
 }
 
@@ -18,7 +18,7 @@ function renderCoffees(coffees) {
     return html;
 }
 
-function updateCoffees(e) {
+function updateCoffees(e) { // Dynamically adjusts display based on roast type selected in dropdown
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
@@ -30,7 +30,7 @@ function updateCoffees(e) {
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
-function updateCoffee2(e) {
+function updateCoffee2(e) { // Dynamically adjusts display based on letters typed in search bar
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedCoffee = searchBar.value;
     var filteredCoffees = [];
@@ -42,24 +42,24 @@ function updateCoffee2(e) {
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
-
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'Light', img: '<img src="coffee-mug.png" class="coffee-mug" alt=""/>'},
+    {id: 2, name: 'Half City', roast: 'Light', img: '<img src="coffee-mug.png" class="coffee-mug" alt=""/>'},
+    {id: 3, name: 'Cinnamon', roast: 'Light', img: '<img src="coffee-mug.png" class="coffee-mug" alt="">'},
+    {id: 4, name: 'City', roast: 'Medium', img: '<img src="coffee-mug.png" class="coffee-mug" alt="">'},
+    {id: 5, name: 'American', roast: 'Medium', img: '<img src="coffee-mug.png" class="coffee-mug" alt="">'},
+    {id: 6, name: 'Breakfast', roast: 'Medium', img: '<img src="coffee-mug.png" class="coffee-mug" alt="">'},
+    {id: 7, name: 'High', roast: 'Dark', img: '<img src="coffee-mug.png" class="coffee-mug" alt="">'},
+    {id: 8, name: 'Continental', roast: 'Dark', img: '<img src="coffee-mug.png" class="coffee-mug" alt="">'},
+    {id: 9, name: 'New Orleans', roast: 'Dark', img: '<img src="coffee-mug.png" class="coffee-mug" alt="">'},
+    {id: 10, name: 'European', roast: 'Dark', img: '<img src="coffee-mug.png" class="coffee-mug" alt="">'},
+    {id: 11, name: 'Espresso', roast: 'Dark', img: '<img src="coffee-mug.png" class="coffee-mug" alt="">'},
+    {id: 12, name: 'Viennese', roast: 'Dark', img: '<img src="coffee-mug.png" class="coffee-mug" alt="">'},
+    {id: 13, name: 'Italian', roast: 'Dark', img: '<img src="coffee-mug.png" class="coffee-mug" alt="">'},
+    {id: 14, name: 'French', roast: 'Dark', img: '<img src="coffee-mug.png" class="coffee-mug" alt="">'},
 ];
+
 coffees.reverse();
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
