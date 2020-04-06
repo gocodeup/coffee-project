e4657ccc3727615b0396639fe6eb897650e4b9a7
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-04-06 13:26:14 -0500

    added comments

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 // Tables are a little old school, you need to refactor the code so that each coffee is displayed in a div that contains a heading displaying the coffee name, and the type of roast in a paragraph. Don't display the ids, these are only for our application's internal use
 //
 
@@ -14,8 +14,6 @@ function renderCoffee(coffee) {
 
 
 //When the page loads, the coffees should be sorted by their ids in ascending order
-
-
 
 function renderCoffees(coffees) {
     var html = '';
