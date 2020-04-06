f9bacceef37e3ea5d1e85dcc773f9b701aa0adb6
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-06 18:37:37 -0500

    Feat: added some dotten lines at the bottom but had to use the js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 "use strict";
 
 function renderCoffee(coffee) {
-    var html = '<div id="coffee-list" class="d-flex coffee" style="width: 260px; height: 75px;">';
+    var html = '<div id="coffee-list" class="coffee mb-5" style="width: 260px; height: 60px;">';
     html += '<h3>' + coffee.name + '</h3>';
     html += '<p>' + coffee.roast + '</p>';
-    html += '</div>';
+    html += '<div>.........................................................</div></div>';
 
     return html;
 }
