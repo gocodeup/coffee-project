bfa0f11a6884970f07c8a38d9b4bb3435dfaa9a2
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2020-11-05 17:41:26 -0600

    messed up

--- main.js
+++ main.js
@@ -1,15 +1,67 @@
-"use strict"
-
+use strict"
 function renderCoffee(coffee) {
     var html = '<tr class="coffee">';
     html += '<td>' + coffee.id + '</td>';
     html += '<td>' + coffee.name + '</td>';
     html += '<td>' + coffee.roast + '</td>';
     html += '</tr>';
-
     return html;
 }
+function renderCoffees(coffees) {
+    var html = '';
+    for(var i = coffees.length - 1; i >= 0; i--) {
+        html += renderCoffee(coffees[i]);
+    }
+    return html;
+}
+function updateCoffees(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSelection.value;
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast) {
+            filteredCoffees.push(coffee);
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+}
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+var coffees = [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
+var tbody = document.querySelector('#coffees');
+var submitButton = document.querySelector('#submit');
+var roastSelection = document.querySelector('#roast-selection');
+tbody.innerHTML = renderCoffees(coffees);
+submitButton.addEventListener('click', updateCoffees);
 
+
+
+
+
+5:36
+"use strict"
+function renderCoffee(coffee) {
+    var html = '<tr class="coffee">';
+    html += '<td>' + coffee.id + '</td>';
+    html += '<td>' + coffee.name + '</td>';
+    html += '<td>' + coffee.roast + '</td>';
+    html += '</tr>';
+    return html;
+}
 function renderCoffees(coffees) {
     var html = '';
     for(var i = coffees.length - 1; i >= 0; i--) {
@@ -29,7 +81,6 @@ function updateCoffees(e) {
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
-
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
@@ -47,15 +98,11 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
-
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
-
 tbody.innerHTML = renderCoffees(coffees);
-
 submitButton.addEventListener('click', updateCoffees);
-
 var roastAnchors = document.getElementsByClassName("dropdown-item")
 for (let roast of roastAnchors){
     roast.addEventListener("click", function () {
@@ -63,17 +110,20 @@ for (let roast of roastAnchors){
         console.log(this.getAttribute("data-roast"));
         if (this.getAttribute("data-roast") === "all"){
             // var tbody = document.querySelector('#coffees');
-        //    display all
+            //    display all
+            submitButton.addEventListener('click', updateCoffees);
         }
         else if (this.getAttribute("data-roast") === "light"){
-
+            submitButton.addEventListener('click', updateCoffees);
             //    display all
         }
         else if (this.getAttribute("data-roast") === "medium"){
             //    display all
+            submitButton.addEventListener('click', updateCoffees);
         }
         else if (this.getAttribute("data-roast") === "dark"){
             //    display all
+            submitButton.addEventListener('click', updateCoffees);
         }
     })
 }
