6db45cbc2e1716c2c08dfdc6180dc08682261b55
Christian Parker <christianparker512@gmail.com>
2020-11-05 17:02:21 -0600

    1st Coffee push

--- main.js
+++ main.js
@@ -1,8 +1,53 @@
 "use strict"
+var coffees = [
+    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 2, name: 'Half City', roast: 'light'},
+    {id: 3, name: 'Cinnamon', roast: 'light'},
+    {id: 4, name: 'City', roast: 'medium'},
+    {id: 5, name: 'American', roast: 'medium'},
+    {id: 6, name: 'Breakfast', roast: 'medium'},
+    {id: 7, name: 'High', roast: 'dark'},
+    {id: 8, name: 'Continental', roast: 'dark'},
+    {id: 9, name: 'New Orleans', roast: 'dark'},
+    {id: 10, name: 'European', roast: 'dark'},
+    {id: 11, name: 'Espresso', roast: 'dark'},
+    {id: 12, name: 'Viennese', roast: 'dark'},
+    {id: 13, name: 'Italian', roast: 'dark'},
+    {id: 14, name: 'French', roast: 'dark'},
+];
+
+// var chooseDate = document.getElementById("chooseDate")
+// function logDay () {
+//     console.log(chooseDate.getMonth())
+// }
+// setInterval(logDay, 2000)
+// if (chooseDate.value )
+var date = new Date(chooseDate)
+var espresso = document.getElementById("Espresso")
+var coffeeName = document.getElementById("coffeeName")
+
+espresso.addEventListener("click", function(){
+    console.log("you clicked me!")
+    var coffee = coffeeName.value
+    var coffeeNameStr = coffee.toString()
+    console.log(coffeeNameStr)
+    if (coffees.name === coffeeNameStr) {
+        console.log(coffees.roast)
+    }
+})
+
+
+
+
+
+
+
+
+
 
 function renderCoffee(coffee) {
     var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
+    // html += '<td>' + coffee.id + '</td>';
     html += '<td>' + coffee.name + '</td>';
     html += '<td>' + coffee.roast + '</td>';
     html += '</tr>';
@@ -31,22 +76,7 @@ function updateCoffees(e) {
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
+
 
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
