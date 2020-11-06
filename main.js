2a3bd059167472a7560e0dccc497237ceee47400
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-11-06 11:37:41 -0600

    refactored and cleaned css

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 function renderCoffee(coffee){
     var html = '<div class="coffee">';
-    html += '<div>' + '<h3>' + coffee.name + '</h3>' + " " + '<p>' + coffee.roast + '</p>' + " " +  coffee.id + '</div>';
+    html += '<div>' + '<h3>' + coffee.name + '</h3>' + " " + '<p>' + coffee.roast + '</p>' + '</div>';
     html += '</div>'
     return html;
 }
