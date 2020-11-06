9b0e11be7d15dc5905a1e6648e6df193a3959bcb
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-11-06 09:35:10 -0600

    completed TODOS 1-4

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 "use strict"
 
+//displaying the table on the page
 function renderCoffee(coffee) {
     var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
+    // html += '<td>' + coffee.id + '</td>';
     html += '<td>' + coffee.name + '</td>';
     html += '<td>' + coffee.roast + '</td>';
     html += '</tr>';
@@ -10,14 +11,17 @@ function renderCoffee(coffee) {
     return html;
 }
 
+
+//ascending the order of the coffee names/list
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    for(var i = 0; i <= coffees.length - 1; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
 
+//updating the table of coffees on the page
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
@@ -30,6 +34,9 @@ function updateCoffees(e) {
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
+
+
+//The array of objects that contains information about each coffee choice
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
@@ -48,10 +55,45 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
+
+//is assigning the content within an id of #coffees to the tbody variable (the actual table body of the table)
 var tbody = document.querySelector('#coffees');
+
+
+//is assigning the content related to the id of #submit to the submitButton variable (that is what is making it possible to change the content on the page)
 var submitButton = document.querySelector('#submit');
+
+
+
+//is assigning the content related to the id of #roast-selection to the roastSelection variable
 var roastSelection = document.querySelector('#roast-selection');
 
+
+//changing the innerHTML of tbody using the function below
 tbody.innerHTML = renderCoffees(coffees);
 
+
+//Event listener for the submitButton(that is what is making the button actually work)
 submitButton.addEventListener('click', updateCoffees);
+
+
+
+//Function for Filter Table(searching through specific data in a table)
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("coffee-search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("coffee-table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
