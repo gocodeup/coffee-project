cbb73db77b5a590180a2c0d4f5a3bdf9e641eab6
kuwan barnes <Kuwan.barne@gmail.com>
2020-12-15 12:07:23 -0600

    workingsearchbarv1

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 "use strict"
-
 function renderCoffee(coffee) {
     var html = '<div class="coffee">';
     html += '<div class="col d-none"> ' + coffee.id + '</div>';
     html += '<h3>' + coffee.name + '</h3>';
     html += '<p>' + coffee.roast + '</p>';
     html += '</div>';
-
     return html;
 }
-
 function renderCoffees(coffees) {
     var html = '';
     for(var i = coffees.length - 1; i >= 0; i--) {
@@ -17,47 +14,48 @@ function renderCoffees(coffees) {
     }
     return html;
 }
-
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
+    //var selectedCoffeeName = coffeeNameSelection.value;
     var filteredCoffees = [];
-    var finalcoffee=[];
+    var finalCoffee=[];
     coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
+        if (coffee.roast === selectedRoast || selectedRoast === 'All') {
             filteredCoffees.push(coffee);
         }
     });
-    // var searchText = document.querySelector('#coffeeDataList');
+    //coffeeSearch.innerHTML = renderCoffees(filteredCoffees);
     var searchText = document.forms.coffeeSearch.searchText.value;
-    console.log(searchText);
-
-    filteredCoffees.forEach(function (){
-        // if(coffee.name === searchText)
-    })
-
-    coffeeSearch.innerHTML = renderCoffees(filteredCoffees);
+    //console.log(searchText);
+    coffees.forEach(function (coffee ) {
+        if(coffee.name.toLowerCase() == searchText.toLowerCase()){
+            finalCoffee.push(coffee);
+            console.log(finalCoffee);
+            //console.log(coffee.name);
+        }
+    });
+    if (finalCoffee.length >= 1)  {
+        coffeeSearch.innerHTML = renderCoffees(finalCoffee);
+    } else {
+        coffeeSearch.innerHTML = renderCoffees(filteredCoffees);
+    }
+    //renderCoffee(finalCoffee);
 }
-
-// function searchCoffees() {
-//     // Declare variables
-//     var input, filter, coffeeName, a, i, txtValue;
-//     input = document.getElementById('coffees');
-//     filter = input.value.toUpperCase();
-//     coffeeName = document.getElementsByTagName('h3');
-//
-//     // Loop through all list items, and hide those who don't match the search query
-//     for (i = 0; i < li.length; i++) {
-//         a = coffeeName[i].getElementsByTagName("a")[0];
-//         txtValue = a.textContent || a.innerText;
-//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//             coffeeName[i].style.display = "";
-//         } else {
-//             coffeeName[i].style.display = "none";
-//         }
-//     }
+for(var i = 0; i <= 10; i++) {
+    console.log(i);
+}
+// if(selectedCoffeeName == filteredCoffee.name){
+//   finalCoffee.push(filteredCoffee);
 // }
-
+//search coffee
+// var searchText = document.forms.coffeeSearch.searchText.value;
+//
+// const searchCoffee = document.forms['coffees'].querySelector();
+// searchCoffee.addEventListener('keyUp', function (e){
+//   const term = e.target.searchText.toLowerCase();
+//   const coffeeName = coffees.getElementById('h3');
+// })
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
@@ -75,20 +73,15 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
-
 var coffeeSearch = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
-//this variable holds the text that was typed in for the search
-
-
+var coffeeNameSelection =document.querySelector('#name-selection');
 coffeeSearch.innerHTML = renderCoffees(coffees);
-
-
 submitButton.addEventListener('click', updateCoffees);
-
-var myDropdown = document.getElementById('coffeeDataList')
-myDropdown.addEventListener('show.bs.dropdown', function () {
-    // do something...
-})
-S
+coffeeNameSelection.addEventListener('keyup',updateCoffees);
+//
+// var myDropdown = document.getElementById('coffeeDataList')
+// myDropdown.addEventListener('show.bs.dropdown', function () {
+//   // do something...
+// })
