61475415af267c337a58a055371340f714d5d2fb
Roland Valdez <valdez.roland.23@gmail.com>
2021-03-30 21:41:35 -0500

    added submit new coffe and auto display

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 "use strict"
+
 function coffeSelection() {
     var selectedRoast = roastSelection.value;
    var filteredCoffees = [];
    if (selectedRoast === 'all') {
-       filteredCoffees = coffees
+       filteredCoffees = coffees;
    } else {
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
@@ -15,26 +16,41 @@ function coffeSelection() {
        return a.id - b.id;
     });
     menu.innerHTML = renderCoffees(filteredCoffees);
+   //  renderCoffees(filteredCoffees);
 }
 
 function autoType(){
-
-    var selectionInput = coffees.filter(letter => {
-        return letter.roast[0] === coffeeAdder[0];
+    var filteredCoffees = [];
+    if (coffeeAdder.value === ""){
+        filteredCoffees = coffees;
+    }else {
+        for(var i = 0; i < coffees.length; i++) {
+            if((coffees[i].name[0]).toLowerCase() === (coffeeAdder.value[0]).toLowerCase()){
+                filteredCoffees.push(coffees[i]);
+            }
+        }
+    }
+    filteredCoffees.sort(function (b,a){
+        return a.id - b.id;
     });
-    console.log(selectionInput); // [{name: "Nepal", continent: "Asia"}]
+    menu.innerHTML = renderCoffees(filteredCoffees);
+    // renderCoffees(filteredCoffees);
 }
 
+
 function renderCoffee(coffee) {
-    var html = '<div class="coffee">';
-    html += '<h5>' + coffee.name + '</h5>';
+    var html = '<div class="coffee d-flex ">';
+    html += '<h5 class="mx-2 d-flex align-content-center">' + coffee.name + '</h5>';
     html += '<p>' + coffee.roast + '</p>';
     html += '</div>';
-
     return html;
 }
 
+
 function renderCoffees(coffees) {
+    // coffees.sort(function (b,a){
+    //     return a.id - b.id;
+    // });
     var html = '';
     for(var i = coffees.length - 1; i >= 0; i--) {
         html += renderCoffee(coffees[i]);
@@ -42,16 +58,19 @@ function renderCoffees(coffees) {
     return html;
 }
 
+
 function updateCoffees(e) {
+    debugger;
+    startingID += 1;
     e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
-        if (coffee.roast[0] === selectedRoast[0]) {
-            filteredCoffees.push(coffee);
-        }
+    var id = startingID;
+    var roast = roastSelection2.value;
+    var name = extraCoffee.value;
+    coffees.push({id, name, roast});
+    coffees.sort(function (b,a){
+        return a.id - b.id;
     });
-    menu.innerHTML = renderCoffees(filteredCoffees);
+    menu.innerHTML = renderCoffees(coffees);
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
@@ -71,15 +90,18 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
+var startingID = 14;
 
 var menu = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
+var roastSelection2 = document.querySelector('#roast-selection2');
 var coffeeAdder = document.querySelector('#addCoffee');
+var extraCoffee = document.querySelector('#extraCoffee');
 
 roastSelection.addEventListener("change", coffeSelection);
 coffeeAdder.addEventListener('input', autoType);
 
 menu.innerHTML = renderCoffees(coffees);
 
-// submitButton.addEventListener('click', updateCoffees);
+submitButton.addEventListener('click', updateCoffees);
