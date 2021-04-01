e180a94ae664efde7ebd724636d30464e98ac15e
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-01 14:11:46 -0500

    added 'all' button

--- main.js
+++ main.js
@@ -32,8 +32,11 @@ function updateCoffees(e) {
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
     coffees.forEach(function (coffee) {
-        if (coffee.roast === selectedRoast) {
+        if (coffee.roast === selectedRoast || coffee.all === selectedRoast) {
             filteredCoffees.push(coffee);
+        // } else if (coffee.roast !== selectedRoast){
+            // filteredCoffees.push(coffee);
+
         }
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
@@ -56,20 +59,20 @@ function updateCoffees1(e) {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light', all: 'all'},
+    {id: 2, name: 'Half City', roast: 'light', all: 'all'},
+    {id: 3, name: 'Cinnamon', roast: 'light', all: 'all'},
+    {id: 4, name: 'City', roast: 'medium', all: 'all'},
+    {id: 5, name: 'American', roast: 'medium', all: 'all'},
+    {id: 6, name: 'Breakfast', roast: 'medium', all: 'all'},
+    {id: 7, name: 'High', roast: 'dark', all: 'all'},
+    {id: 8, name: 'Continental', roast: 'dark', all: 'all'},
+    {id: 9, name: 'New Orleans', roast: 'dark', all: 'all'},
+    {id: 10, name: 'European', roast: 'dark', all: 'all'},
+    {id: 11, name: 'Espresso', roast: 'dark', all: 'all'},
+    {id: 12, name: 'Viennese', roast: 'dark', all: 'all'},
+    {id: 13, name: 'Italian', roast: 'dark', all: 'all'},
+    {id: 14, name: 'French', roast: 'dark', all: 'all'},
 ];
 
 var searchBar = document.querySelector('#search');
