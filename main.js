2dedbe03c503b692de8a966899bdf35d7a4e01a5
Kenyon Luce <kenyon.luce02@gmail.com>
2021-07-21 13:12:25 -0500

    added add roast function and reorganized code

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function renderCoffees(coffees) {
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
+    var selectedRoast = roastSearch.value;
     var filteredCoffees = [];
     coffees.forEach(function (coffee) {
         if (coffee.roast === selectedRoast || coffee.all === selectedRoast) {
@@ -42,20 +42,6 @@ function updateCoffees(e) {
     tbody.innerHTML = renderCoffees(filteredCoffees);
 
 }
-
-function updateCoffees1(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection1.value;
-    var filteredCoffees = [];
-    coffees.forEach(function (coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        }
-    });
-    tbody1.innerHTML = renderCoffees(filteredCoffees);
-
-}
-
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
@@ -75,8 +61,6 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark', all: 'all'},
 ];
 
-var searchBar = document.querySelector('#search');
-
 function coffeeLoop(event) {
     event.preventDefault(); // don't submit the form, we just want to update the data
     //keeps page from refreshing after submitting (by default)
@@ -91,39 +75,33 @@ function coffeeLoop(event) {
         }
     }
     tbody.innerHTML = renderCoffees(filteredCoffeeNames);
-    tbody1.innerHTML = renderCoffees(filteredCoffeeNames);
 }
 
-var searchBar1 = document.querySelector('#search1');
-
-function coffeeLoop1(event) {
-    event.preventDefault(); // don't submit the form, we just want to update the data
-    //keeps page from refreshing after submitting (by default)
-    var coffeeSearch = searchBar1.value.toLowerCase();
-    var filteredCoffeeNames = [];
-    for (var i = 0; i < coffees.length; i++) {
-        var coffeeName = coffees[i].name;
-        if (coffeeName.toLowerCase().indexOf(coffeeSearch) >= 0) {
-            //happy path, indexOf will return number above or equal to 0 if the typed input exists in any of the coffee names
-            filteredCoffeeNames.push(coffees[i]);
-            //pushes coffee names into filteredCoffeeNames is condition is met
-        }
-    }
-    tbody.innerHTML = renderCoffees(filteredCoffeeNames);
-    tbody1.innerHTML = renderCoffees(filteredCoffeeNames);
-}
-
+//RENDER COFFEES
 var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
-
-var tbody1 = document.querySelector('#coffees1');
-var submitButton1 = document.querySelector('#submit1');
-var roastSelection1 = document.querySelector('#roast-selection1');
 
 tbody.innerHTML = renderCoffees(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
-submitButton1.addEventListener('click', updateCoffees1);
-searchBar.addEventListener('keyup', coffeeLoop);
-searchBar1.addEventListener('keyup', coffeeLoop1);
+//SEARCH COFFEES
+var searchBar = document.querySelector('#name-search');
+var roastSearch = document.querySelector('#roast-search');
+var submitSearch = document.querySelector('#search');
+
+searchBar.addEventListener('keyup', coffeeLoop); //filters as you type
+submitSearch.addEventListener('click', updateCoffees); //retreats to show matching roasts if search query isn't found
+
+//ADD COFFEES
+var addCoffeeName = document.querySelector('#name-add');
+var addCoffeeRoast = document.querySelector('#roast-add');
+var submitAdd = document.querySelector('#add');
+
+submitAdd.addEventListener('click', function(){
+    coffees.push(
+        {
+            id: coffees.length + 1,
+            name: addCoffeeName,
+            roast: addCoffeeRoast,
+            all: 'all'
+        }
+    )
+})
