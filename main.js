1e23faf76cdcd07192628c2f6c3d596ef9c01ae5
Albert Dominguez <albert.j.dominguez21@gmail.com>
2021-08-10 17:56:59 -0500

    added option for all roasts, added option to search by name, search by all, search by roast with no name, refactor

--- main.js
+++ main.js
@@ -1,35 +1,47 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
+    var html = '';
+    //Render an h1 inside a div instead tr
     html += '<div>' + '<h1>' + coffee.name + '</h1>' + '<p>' +  coffee.roast + "</p>" + '</div>';
-    // html += '<td>' + coffee.name + '</td>';
-    // html += '<td>' + coffee.roast + '</td>';
-    // html += '</tr>';
 
     return html;
 }
 
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
-        html += renderCoffee(coffees[i]);
-    }
-    return html;
-}
-
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
+        //Changed order from last to first, to first to last
+        for (var i = 0; i < coffees.length; i++) {
+            html += renderCoffee(coffees[i]);
         }
-    });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
-}
+        return html;
+    }
 
+    function updateCoffees(e) {
+        e.preventDefault(); // don't submit the form, we just want to update the data
+        var selectedRoast = roastSelection.value;
+        var selectedName = nameSelection.value.toLowerCase();
+        var filteredCoffees = [];
+        coffees.forEach(function (coffee) {
+            //If selected roast is all, and no search name, display all
+            if (selectedRoast === "all" && selectedName === "") {
+                filteredCoffees.push(coffee)
+            }
+            //If the selected roast is all, display matched names
+            if (selectedRoast === "all" && coffee.name.toLowerCase() === selectedName) {
+                filteredCoffees.push(coffee)
+            }
+            //Checks if the name and roast match search selections
+            if (coffee.roast === selectedRoast && coffee.name.toLowerCase() === selectedName) {
+                filteredCoffees.push(coffee)
+            }
+            //if name is blank, display selected roast
+            if (selectedName === "" && coffee.roast === selectedRoast) {
+                filteredCoffees.push(coffee)
+            }
+        });
+        divBody.innerHTML = renderCoffees(filteredCoffees);
+    }
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
@@ -47,11 +59,13 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
-
-var tbody = document.querySelector('#coffees');
+//Change tbody to div body
+var divBody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
+//Added name selection search to the form
+var nameSelection = document.querySelector('#coffee-name');
 
-tbody.innerHTML = renderCoffees(coffees);
+divBody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
