ac2d3318f1fec5faf76f9470a9f47f0cb64726ee
Christina Nuncio <nuncio.christina01@gmail.com>
2021-08-11 12:29:28 -0500

    dowloaded library js link

--- main.js
+++ main.js
@@ -30,20 +30,20 @@ function updateCoffees(e) {
     div.innerHTML = renderCoffees(filteredCoffees);
 }
 
-// function searchCoffees() {
-//     let input = document.getElementById('search-btn').value
-//     input=input.toLowerCase();
-//     let x = document.getElementsByClassName('coffee');
-//
-//     for (var i = 0; i < x.length; i++) {
-//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
-//             x[i].style.display="none";
-//         }
-//         else {
-//             x[i].style.display="div";
-//         }
-//     }
-// }
+function searchCoffees() {
+    let input = document.getElementById('search-btn').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('coffee');
+
+    for (var i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="div";
+        }
+    }
+}
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
