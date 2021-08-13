65bfe182adcdbab411310902fae44f8a0ed96125
Kristen Wilson <wilson.kristen614@gmail.com>
2021-08-13 12:04:13 -0500

    final commit before presentation.

--- main.js
+++ main.js
@@ -11,53 +11,50 @@ function renderCoffee(coffee) {
 }
 
 
-// function renderCoffee(coffee) {
-//     // var cinnamonColor = '';
-//     // var lightRoastBG = '';
-//     // if(coffee.roast === 'light'){
-//     //     lightRoastBG = 'bg-warning';
-//     // }
-//     // var mediumRoastBG = '';
-//     // if(coffee.roast === 'medium'){
-//     //     mediumRoastBG = 'bg-info';
-//     // }
-//     // var darkRoastBG = '';
-//     // if(coffee.roast === 'dark'){
-//     //     darkRoastBG = 'bg-secondary';
-//     // }
-//     // if (coffee.name === 'Cinnamon'){
-//     //     cinnamonColor = 'text-danger'
-//     // }
-//
-//
-//    return `<div class="d-flex flex-column border border-secondary rounded-bottom" style="width: 10rem; margin: 2rem;">
-// <!--<div class="card-shape">-->
-//         <img src="img/coffee-cup.jpeg" class="card-img-top"  alt="${coffee.name}">
-// <!--                <div class="container-cards">-->
-// <!--                    <div class="cards row ">-->
-//                 <div class="card-body ${lightRoastBG} ${mediumRoastBG} ${darkRoastBG}">
-//                     <h5 class="card-title ${cinnamonColor}"> ${coffee.name} </h5>
-//                     <p class="card-text">${coffee.roast} </p>
-//                 </div>
-//                     </div>
-//                 </div>
-//            </div>
-//            </div>`
-//     // var html = '<tr class="coffee">';
-//     // html += '<td>' + coffee.id + '</td>';
-//     // html += '<td>' + coffee.name + '</td>';
-//     // html += '<td>' + coffee.roast + '</td>';
-//     // html += '</tr>';
-//
-//     // return html;
-// }
+function renderCoffee(coffee) {
+    var cinnamonColor = '';
+    var lightRoastBG = '';
+    if(coffee.roast === 'light'){
+        lightRoastBG = 'bg-warning';
+    }
+    var mediumRoastBG = '';
+    if(coffee.roast === 'medium'){
+        mediumRoastBG = 'bg-info';
+    }
+    var darkRoastBG = '';
+    if(coffee.roast === 'dark'){
+        darkRoastBG = 'bg-secondary';
+    }
+    if (coffee.name === 'Cinnamon'){
+        cinnamonColor = 'text-danger'
+    }
+
+
+   return `<div class="d-flex flex-column border border-secondary rounded-bottom" style="width: 10rem; margin: 2rem;">
+<div class="card-shape">
+        <img src="img/coffee-cup.jpeg" class="card-img-top"  alt="${coffee.name}">
+                <div class="container-cards">
+                    <div class="cards row ">
+                <div class="card-body ${lightRoastBG} ${mediumRoastBG} ${darkRoastBG}">
+                    <h5 class="card-title ${cinnamonColor}"> ${coffee.name} </h5>
+                    <p class="card-text">${coffee.roast} </p>
+                </div>
+                    </div>
+                </div>
+           </div>
+           </div>`
+    // var html = '<tr class="coffee">';
+    // html += '<td>' + coffee.id + '</td>';
+    // html += '<td>' + coffee.name + '</td>';
+    // html += '<td>' + coffee.roast + '</td>';
+    // html += '</tr>';
+    //
+    // return html;
+}
 
 
 
 function renderCoffees(coffees) {
-    // return coffees.reduce((coffeeHTML, coffee) => coffeeHTML.concat(renderCoffee(coffee)), '' )
-
-
 
     var html = '';
     for(var i = coffees.length - 1; i >= 0; i--) {
@@ -113,45 +110,54 @@ submitButton.addEventListener('click', updateCoffees);
 
 
 
-
-
-
-coffees.forEach(function (coffee, index){
-    console.log(coffees.name + coffees.roast)
-})
-
-
-function createCoffee (nameOfCoffee, roastAdded){
-    this.name = nameOfCoffee;
-    this.roast = roastAdded;
-
+function createCoffee (nameOfCoffee) {
+    var nameOfCoffee = nameOfCoffee.split('');
+    var coffeeNameSplit = {};
+    for (var i = 0; i < nameOfCoffee.length; i++)
+        coffeeNameSplit.push = ({
+            name: nameOfCoffee[i],
+            roast: nameOfCoffee[i]
+        })
 }
-var newCoffee = new createCoffee('Nachos', 'Light')
+    console.log(createCoffee('kris light'));
 
 
-console.log(newCoffee('Nachos', 'light'))
+// coffees.forEach(function (coffee, index){
+//     console.log(coffees.name + coffees.roast)
+// })
+//
+//
+// function createCoffee (nameOfCoffee, roastAdded){
+//     this.name = nameOfCoffee;
+//     this.roast = roastAdded;
+//
+// }
+// var newCoffee = new createCoffee('Nachos', 'Light')
+//
+//
+// console.log(newCoffee('Nachos', 'light'))
 
-function myFunction() {
-    // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
+    function myFunction() {
+        // Declare variables
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
 
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-    txtValue = td.textContent || td.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-    tr[i].style.display = "";
-} else {
-    tr[i].style.display = "none";
-}
-}
-}
-}
+        // Loop through all table rows, and hide those who don't match the search query
+        for(i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if(td) {
+                txtValue = td.textContent || td.innerText;
+                if(txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
 
 
 
