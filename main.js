5d06b0986207daccb42cd1884a8aa73c12e58d57
daevon johnson <daevontjohnson01@gmail.com>
2021-11-17 12:30:52 -0600

    committed

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<div class="coffee">';
+    var html = '<div class="coffee d-inline-flex align-self-stretch col-6" id="mylist">';
     // html += '<td>' + coffee.id + '</td>';
-    html += '<div class="font-weight-bold">' + coffee.name + '</div>';
-    html += '<div class="font-weight-light">' + coffee.roast + '</div>';
+    html += '<div class="font-weight-bold h1 mb-2 ">' + coffee.name + '</div>';
+    html += '<div class="font-weight-light h4 pt-3 ml-2 text-secondary">' + coffee.roast + '</div>';
     html += '</div>';
 
     return html;
@@ -30,7 +30,6 @@ function updateCoffees(e) {
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
-
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
@@ -50,9 +49,7 @@ var coffees = [
 ];
 
 var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+var submitButton = document.getElementById('ControlInput1');
+var roastSelection = document.getElementById('roast-selection');
 
-tbody.innerHTML = renderCoffees(coffees);
-
-submitButton.addEventListener('click', updateCoffees);
+submitButton.addEventListener('keyup', updateCoffees);
