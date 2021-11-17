6f19698a33d64eb34657e31b254a6d0faadb6670
Nicholas <nicholasdiazjr509@gmail.com>
2021-11-17 11:03:21 -0600

    fixed a lot of the code

--- main.js
+++ main.js
@@ -38,12 +38,13 @@ function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
+     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
         }
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
+        return filteredCoffees;
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
