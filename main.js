d1d855a68f0fe8fb4f61581aba0cdf36e9c12184
Jennifer Deleon <91964802+JennDeleon404@users.noreply.github.com>
2021-11-17 18:47:16 -0600

    added coffee details hover

--- main.js
+++ main.js
@@ -3,18 +3,21 @@
 function renderCoffee(coffee) {
     if (coffee.roast === 'light') {
         var html = '<div class="coffee light">';
-        html += '<h1>' + coffee.name + '</h1>';
-        html += '<p>' + coffee.roast + '</p>';
+        html += '<h2>' + coffee.name + '</h2>';
+        html += '<div class="roast"><p>' + coffee.roast + '</p></div>';
+        html += '<div class="hide"><h4>' + coffee.description + '</h4></div>';
         html += '</div>';
     } else if ( coffee.roast === 'medium'){
         var html = '<div class="coffee medium">';
-        html += '<h1>' + coffee.name + '</h1>';
-        html += '<p>' + coffee.roast + '</p>';
+        html += '<h2>' + coffee.name + '</h2>';
+        html += '<div class="roast"><p>' + coffee.roast + '</p></div>';
+        html += '<div class="hide"><h4>' + coffee.description + '</h4></div>';
         html += '</div>';
     } else if (coffee.roast === 'dark') {
         var html = '<div class="coffee dark">';
-        html += '<h1>' + coffee.name + '</h1>';
-        html += '<p>' + coffee.roast + '</p>';
+        html += '<h2>' + coffee.name + '</h2>';
+        html += '<div class="roast"><p>' + coffee.roast + '</p></div>';
+        html += '<div class="hide"><h4>' + coffee.description + '</h4></div>';
         html += '</div>';
     }
     return html;
@@ -86,20 +89,20 @@ function searchCoffees(y) {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light', description: 'Light flavor with bright acidic notes'},
+    {id: 2, name: 'Half City', roast: 'light', description: 'Medium flavor with bright acidic notes'},
+    {id: 3, name: 'Cinnamon', roast: 'light', description: 'With a hint of sweetness, mellow and cozey'},
+    {id: 4, name: 'City', roast: 'medium', description: 'Full flavor with bright acidic notes'},
+    {id: 5, name: 'American', roast: 'medium', description: 'Notes of cocoa and toasted nuts'},
+    {id: 6, name: 'Breakfast', roast: 'medium', description: 'Mild roast, smooth with a citrusy tang'},
+    {id: 7, name: 'High', roast: 'dark', description: 'Robust, full body'},
+    {id: 8, name: 'Continental', roast: 'dark', description: 'Full body with a fruity nuance'},
+    {id: 9, name: 'New Orleans', roast: 'dark', description: 'Deep dark blend with smooth smokey flavors'},
+    {id: 10, name: 'European', roast: 'dark', description: 'Robust with s distinct flavor'},
+    {id: 11, name: 'Espresso', roast: 'dark', description: 'Luxuriously creamy, with a kiss of foamy cream'},
+    {id: 12, name: 'Viennese', roast: 'dark', description: 'Espresso with half steamed milk, topped off with milk froth.'},
+    {id: 13, name: 'Italian', roast: 'dark', description: 'Arabica coffee smooth and acidic'},
+    {id: 14, name: 'French', roast: 'dark', description: 'Intense and smokey-sweet flavor'},
 ];
 
 var div = document.querySelector('#coffees');
