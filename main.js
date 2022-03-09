8858b9cad9b6e17352ad5858b20b027c4f457e1a
desktop-o0gpqr4\antsh <ants.hernandez@gmail.com>
2022-03-09 15:36:06 -0600

    Updating Project

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function renderCoffee(coffee) {
 
 function renderCoffees(coffees) {
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    for(var i= 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
