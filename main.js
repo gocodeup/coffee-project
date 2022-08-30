976a58df20ebcc59c1c6951c4bebf87d98c4fd7a
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-08-30 14:50:43 -0500

    var to let

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = `<div class="coffee" id="${coffee.id}">`;
-    // html += '<td>' + coffee.id + '</td>';
+    let html = `<div class="coffee" id="${coffee.id}">`;
     html += '<h2>' + coffee.name + '</h2>';
     html += '<p>' + coffee.roast + '</p>';
     html += '</div>';
@@ -11,17 +10,17 @@ function renderCoffee(coffee) {
 }
 
 function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
-        html += renderCoffee(coffees[i]);
+    let htmlS = '';
+    for(let i = coffees.length - 1; i >= 0; i--) {
+        htmlS += renderCoffee(coffees[i]);
     }
-    return html;
+    return htmlS;
 }
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
+    let selectedRoast = roastSelection.value;
+    let filteredCoffees = [];
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
@@ -31,7 +30,7 @@ function updateCoffees(e) {
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
+let coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
     {id: 3, name: 'Cinnamon', roast: 'light'},
@@ -48,9 +47,9 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
-var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+let tbody = document.querySelector('#coffees');
+let submitButton = document.querySelector('#submit');
+let roastSelection = document.querySelector('#roast-selection');
 
 tbody.innerHTML = renderCoffees(coffees);
 
