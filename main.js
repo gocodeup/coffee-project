51dc72cc3ccfa5bbc9ad8fe4a7d8321337cf82c3
Mia Evans <54636763+miamija7@users.noreply.github.com>
2022-08-31 10:34:01 -0500

    fix updateCoffees function

--- main.js
+++ main.js
@@ -11,64 +11,69 @@ function renderCoffee(coffee) {
 
 function renderCoffees(coffees) {
     let htmlS = '';
-    for(let i = 0; i < coffees.length; i++) {
+    for (let i = 0; i < coffees.length; i++) {
         htmlS += renderCoffee(coffees[i]);
     }
     return htmlS;
 }
+
 function renderOpt(coffee) {
-    let html = `<option value="${coffee.name}">`;
-    return html;
+    return `<option value="${coffee.name}">`;
 }
+
 function renderDatalistOpt(coffees) {
     let htmlS = '';
-    for(let i = 0; i < coffees.length; i++) {
+    for (let i = 0; i < coffees.length; i++) {
         htmlS += renderOpt(coffees[i]);
     }
     return htmlS;
 }
 
-
 function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
+    e.preventDefault(); // don't submit the eform, we just want to update the data
     let selectedRoast = roastSelection.value;
+    let selectedName = nameSelection.value;
     let filteredCoffees = [];
-    let selectName = nameSelection.value;
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        } else if (coffee.name.contains(selectName)) {
-            filteredCoffees.push(coffee)
+
+    coffees.forEach(function (coffee) {
+        if (selectedRoast === "All") selectedRoast = 'Light, Medium, Dark';
+
+        if (!!selectedName) {
+            if (selectedRoast.includes(coffee.roast) && coffee.name.toLowerCase().includes(selectedName.toLowerCase()))
+                filteredCoffees.push(coffee);
+        } else {
+            if (selectedRoast.includes(coffee.roast))
+                filteredCoffees.push(coffee);
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
-}
+        tbody.innerHTML = renderCoffees(filteredCoffees);
+    }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-let coffees = [
-    {id: 1, name: 'Light City', roast: 'Light'},
-    {id: 2, name: 'Half City', roast: 'Light'},
-    {id: 3, name: 'Cinnamon', roast: 'Light'},
-    {id: 4, name: 'City', roast: 'Medium'},
-    {id: 5, name: 'American', roast: 'Medium'},
-    {id: 6, name: 'Breakfast', roast: 'Medium'},
-    {id: 7, name: 'High', roast: 'Dark'},
-    {id: 8, name: 'Continental', roast: 'Dark'},
-    {id: 9, name: 'New Orleans', roast: 'Dark'},
-    {id: 10, name: 'European', roast: 'Dark'},
-    {id: 11, name: 'Espresso', roast: 'Dark'},
-    {id: 12, name: 'Viennese', roast: 'Dark'},
-    {id: 13, name: 'Italian', roast: 'Dark'},
-    {id: 14, name: 'French', roast: 'Dark'},
-];
+    let coffees = [
+        {id: 1, name: 'Light City', roast: 'Light'},
+        {id: 2, name: 'Half City', roast: 'Light'},
+        {id: 3, name: 'Cinnamon', roast: 'Light'},
+        {id: 4, name: 'City', roast: 'Medium'},
+        {id: 5, name: 'American', roast: 'Medium'},
+        {id: 6, name: 'Breakfast', roast: 'Medium'},
+        {id: 7, name: 'High', roast: 'Dark'},
+        {id: 8, name: 'Continental', roast: 'Dark'},
+        {id: 9, name: 'New Orleans', roast: 'Dark'},
+        {id: 10, name: 'European', roast: 'Dark'},
+        {id: 11, name: 'Espresso', roast: 'Dark'},
+        {id: 12, name: 'Viennese', roast: 'Dark'},
+        {id: 13, name: 'Italian', roast: 'Dark'},
+        {id: 14, name: 'French', roast: 'Dark'},
+    ];
 
-let tbody = document.querySelector('#coffees');
-let submitButton = document.querySelector('#submit');
-let roastSelection = document.querySelector('#roast-selection');
-let datalistOptions = document.querySelector('#datalistOptions');
-let nameSelection = document.querySelector('#name-selection');
+    let tbody = document.querySelector('#coffees');
+    let submitButton = document.querySelector('#submit');
+    let roastSelection = document.querySelector('#roast-selection');
+    let datalistOptions = document.querySelector('#datalistOptions');
+    let nameSelection = document.querySelector('#name-selection');
 
-tbody.innerHTML = renderCoffees(coffees);
-datalistOptions.innerHTML = renderDatalistOpt(coffees);
+    tbody.innerHTML = renderCoffees(coffees);
+    datalistOptions.innerHTML = renderDatalistOpt(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
+    submitButton.addEventListener('click', updateCoffees);
