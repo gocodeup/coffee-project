ae26969c1a425bf2db69f0117aad06441e5f3b08
ethan <ethan.gonzalez00774@gmail.com>
2022-10-15 15:39:42 -0700

    finesh division section

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 "use strict"
-
+//creates row for an array in mew page
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+    var html = '<div class="coffee">';
+    // html += '<td>' + coffee.id + '</td>';
+    html += '<h3>' + coffee.name + '</h3>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
 
     return html;
 }
-
+// takes new array and organizes it in mumeric order
 function renderCoffees(coffees) {
     var html = '';
     for(var i = coffees.length - 1; i >= 0; i--) {
@@ -17,22 +17,39 @@ function renderCoffees(coffees) {
     }
     return html;
 }
+// filters coffee and makes new array 1
 
-function updateCoffees(e) {
+function updateCoffees(e) {  //☕️☕️☕️☕️☕️
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
     coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
+        if (coffee.roast === selectedRoast ) {
+            filteredCoffees.push(coffee);
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+}
+function searchCoffees(e) {  //new
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    // var selectedRoast = roastSelection.value;
+    var searchedRoast = roastSearch.value;
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === searchedRoast ) {
             filteredCoffees.push(coffee);
         }
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
+
+
+
+
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
+    {id: 1, name: 'Light City', roast: 'light' },
     {id: 2, name: 'Half City', roast: 'light'},
     {id: 3, name: 'Cinnamon', roast: 'light'},
     {id: 4, name: 'City', roast: 'medium'},
@@ -49,9 +66,16 @@ var coffees = [
 ];
 
 var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+// var submitButton = document.querySelector('#search'); // ☕️☕️☕️☕️☕️
+var roastSelection = document.querySelector('#roast-selection')
+
+
 
 tbody.innerHTML = renderCoffees(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
+// submitButton.addEventListener('click', updateCoffees);
+roastSelection.addEventListener('change', updateCoffees);
+
+var roastSearch = document.querySelector('#roast-search')// new
+roastSearch.addEventListener('search',searchCoffees);//new
+
