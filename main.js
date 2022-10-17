e2f4052683276b4298af7158c127a8ffb68ee702
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-10-17 09:35:44 -0500

    completed conversion from table to div

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 "use strict"
 
+// When the page loads, the coffees should be sorted by their ids in ascending order
+//
+// dd functionality to search through the coffees by name, and display only the coffees that match the provided search term (You will need to add an input field to the existing form for this)
+//
+// Add functionality to update the displayed coffee as the user types into the search box, or as soon as they select an option from the select.
+
+// Add an option to select all roasts for roast type
+//
+// Make your name search case insensitive
+//
+// Allow the user to add new coffees to the page
+//
+// Create another form on the page that has an input for the coffee name, and a select to choose the coffee roast. When the form is submitted, the new coffee should appear on the page. (Note that any new coffees you add will be lost when you refresh the page, for an extra challenge, research how localStorage works and see if you can find a way to persist the data)
+
+
 function renderCoffee(coffee) {
     var html = '<div class="coffee id="' + coffee.id + '">';
     html += '<h2>' + coffee.name + '</h2>';
