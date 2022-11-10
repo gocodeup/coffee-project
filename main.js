73cbc0a7a22871d04564d33b59a1d837a4c7411d
Austin Hunt <austin.hunt.012@gmail.com>
2022-11-10 14:58:06 -0600

    added logos

--- main.js
+++ main.js
@@ -3,20 +3,20 @@
 // Coffee Inventory
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 let coffees = [
-    {id: 1, name: 'Light City', roast: 'light', img: 'img/coffee_electric-unicorn.webp'},
-    {id: 2, name: 'Half City', roast: 'light', img: 'img/coffee_mint-invaders.webp'},
-    {id: 3, name: 'Cinnamon', roast: 'light', img: 'img/coffee_sinn-o-bun.webp'},
-    {id: 4, name: 'City', roast: 'medium', img: 'img/coffee_salty-siren.webp'},
-    {id: 5, name: 'American', roast: 'medium', img: 'img/coffee_dusk-donuts.webp'},
-    {id: 6, name: 'Breakfast', roast: 'medium', img: 'img/coffee_maple-bacon.webp'},
-    {id: 7, name: 'High', roast: 'dark', img: 'img/coffee_high-voltage.webp'},
-    {id: 8, name: 'Continental', roast: 'dark', img: 'img/coffee_macamaniac.webp'},
-    {id: 9, name: 'New Orleans', roast: 'dark', img: 'img/coffee_bananas-foster.webp'},
-    {id: 10, name: 'European', roast: 'dark', img: 'img/coffee_wunderbones.webp'},
-    {id: 11, name: 'Espresso', roast: 'dark', img: 'img/coffee_espresso.webp'},
-    {id: 12, name: 'Viennese', roast: 'dark', img: 'img/coffee_bluesberry.webp'},
-    {id: 13, name: 'Italian', roast: 'dark', img: 'img/coffee_holy-cannoli.webp'},
-    {id: 14, name: 'French', roast: 'dark', img: 'img/coffee_french-vanilla.webp'},
+    {id: 1, name: 'Light City', roast: 'Light', img: 'img/coffee_electric-unicorn.webp'},
+    {id: 2, name: 'Half City', roast: 'Light', img: 'img/coffee_mint-invaders.webp'},
+    {id: 3, name: 'Cinnamon', roast: 'Light', img: 'img/coffee_sinn-o-bun.webp'},
+    {id: 4, name: 'City', roast: 'Medium', img: 'img/coffee_salty-siren.webp'},
+    {id: 5, name: 'American', roast: 'Medium', img: 'img/coffee_dusk-donuts.webp'},
+    {id: 6, name: 'Breakfast', roast: 'Medium', img: 'img/coffee_maple-bacon.webp'},
+    {id: 7, name: 'High', roast: 'Dark', img: 'img/coffee_high-voltage.webp'},
+    {id: 8, name: 'Continental', roast: 'Dark', img: 'img/coffee_macamaniac.webp'},
+    {id: 9, name: 'New Orleans', roast: 'Dark', img: 'img/coffee_bananas-foster.webp'},
+    {id: 10, name: 'European', roast: 'Dark', img: 'img/coffee_wunderbones.webp'},
+    {id: 11, name: 'Espresso', roast: 'Dark', img: 'img/coffee_espresso.webp'},
+    {id: 12, name: 'Viennese', roast: 'Dark', img: 'img/coffee_bluesberry.webp'},
+    {id: 13, name: 'Italian', roast: 'Dark', img: 'img/coffee_holy-cannoli.webp'},
+    {id: 14, name: 'French', roast: 'Dark', img: 'img/coffee_french-vanilla.webp'},
 ];
 
 // Custom Coffee Functionality
