dcea07fcf45da1bcd19483fa616afb87ed81ff88
Herb <herbrholder@gmail.com>
2022-11-11 16:03:45 -0600

    added core functionality

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-"use strict"
 
+
+
+let filteredCoffees = [];
 let coffeeContainer = document.getElementById('coffee-list');
 let searchSubmit = document.getElementById("search-btn");
 let searchCoffee = document.getElementById("coffee-search");
@@ -7,38 +9,55 @@ let roast = document.getElementById("roast-selection"); // our select tag
 searchSubmit.addEventListener("click", updateCoffees);
 roast.addEventListener('change', updateCoffees);
 
-console.log('coffeeContainer: ', coffeeContainer);
-console.log('searchSubmit: ', searchSubmit);
-console.log('searchCoffee: ', searchCoffee);
-console.log('roast: ', roast);
+let typedCoffee = document.getElementById("add-coffee");
+
+
+
+
+
+const input = document.getElementById('add-coffee');
+input.addEventListener('keypress', updateCoffees);
+const log = document.getElementById('coffeeChoice');
+
+
+
+
+
+
+// console.log('coffeeContainer: ', coffeeContainer);
+// console.log('searchSubmit: ', searchSubmit);
+// console.log('searchCoffee: ', searchCoffee);
+// console.log('roast: ', roast);
 
 
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light',image:"assets/nathan-dumlao-zUNs99PGDg0-unsplash.jpeg"},
+    {id: 2, name: 'Half City', roast: 'light',image:""},
+    {id: 3, name: 'Cinnamon', roast: 'light',image:""},
+    {id: 4, name: 'City', roast: 'medium',image:""},
+    {id: 5, name: 'American', roast: 'medium',image:""},
+    {id: 6, name: 'Breakfast', roast: 'medium',image:""},
+    {id: 7, name: 'High', roast: 'dark',image:""},
+    {id: 8, name: 'Continental', roast: 'dark',image:""},
+    {id: 9, name: 'New Orleans', roast: 'dark',image:""},
+    {id: 10, name: 'European', roast: 'dark',image:""},
+    {id: 11, name: 'Espresso', roast: 'dark',image:""},
+    {id: 12, name: 'Viennese', roast: 'dark',image:""},
+    {id: 13, name: 'Italian', roast: 'dark',image:""},
+    {id: 14, name: 'French', roast: 'dark',image:""},
 ];
 
 
+
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td class="coffee-name">' + coffee.name + '</td>';
-    html += '<td class="roast">' + coffee.roast + '</td>';
-    html += '</tr>';
+    var html = `<div class="coffee-cont">`;
+    // html += `<td> + coffee.id + </td>`;
+    html += `<figure class><img width="100px" src="${coffee.image}"/></figure>`
+    html += `<p class="coffee-name">${coffee.name}</p>`;
+    html += `<p class="roast">||${coffee.roast}</p>`;
+    html += `</div>`; 
 
     return html;
 }
@@ -55,10 +74,12 @@ function renderCoffees(coffees) {
 // +++++++++++++++++
 function updateCoffees(e) {
     // console.log('updateCoffees(): ');
-    
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    let filteredCoffees = [];
+    console.log('keydown');
+    let userTyped =  [];
+    filteredCoffees = []; 
+    // e.preventDefault(); // don't submit the form, we just want to update the data
     let selectedRoast = roast.value.toLowerCase()
+    
     for (let i = 0; i < coffees.length;i++)
     {
         console.log('coffee roast', coffees[i].roast);
@@ -70,21 +91,53 @@ function updateCoffees(e) {
             filteredCoffees.push(coffees[i]);
         }
     }
+    console.log(filteredCoffees)
+
+    for (let i = 0; i < filteredCoffees.length;i++){
+        let coffeeName = input.value.toLowerCase();
+        // console.log(coffeeName)
+        // console.log(filteredCoffees[i].name.toLowerCase())
+            if (filteredCoffees[i].name.toLowerCase().includes(coffeeName)){
+                userTyped.push(filteredCoffees[i]);
+                
+            }  
+        }
+
     // console.log("filteredCoffees: ",filteredCoffees);
     // console.log('roast.value', roast.value);
     // console.log("coffees: ", coffees);
+
+
+    if(userTyped !== null){
+        coffeeContainer.innerHTML = renderCoffees(userTyped);
+    }else{
+    coffeeContainer.innerHtml = renderCoffees(filteredCoffees);
+    }
+}
+
+// input.addEventListener('input', function(e) {
+//    let userTyped =  []; 
+//   console.log(filteredCoffees);
+
+//    for (let i = 0; i < filteredCoffees.length;i++){
+//     let coffeeName = input.value.toLowerCase();
+//     console.log(coffeeName)
+//     coneole.log(filteredCoffees[i].toLowerCase())
+//         if (filteredCoffees[i].name.toLowerCase().includes(coffeeName)){
+//             userTyped.push(filteredCoffees[i]);
+            
+//         }
+        
+//         coffeeContainer.innerHTML = userTyped[i];
+        
+//     }
     
-    coffeeContainer.innerHTML = renderCoffees(filteredCoffees);
+//     console.log("userTyped: ",userTyped);
+// // console.log(input.value)
+    
+    
+
+// } )     
 
 
-}
-
-const input = document.querySelector('input');
-const log = document.getElementById('coffeeChoice');
-
-input.addEventListener('input', updateValue);
-
-function updateValue(e) {
-  console.log(e.target.value);
-}
 
