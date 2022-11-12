446628c4b3b1fb5a7a0ea29b20f549383cfd0312
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2022-11-12 08:18:32 -0600

    ...

--- main.js
+++ main.js
@@ -1,21 +1,61 @@
 "use strict"
 
+//+++++++++++++++++++++++++++++++++++++++++---LEFT---++++++++++++++++++++++++++++++++++++++
+
+//Renders coffee on to the empty div
+function renderCoffee(coffee) {
+    let html = '<tr class="coffee">';
+    html += '<td>' + coffee.id + '</td>';
+    html += '<h1 class="coffee-name">' + coffee.name + '</h1>';
+    html += '<p class="roast">' + coffee.roast + '</p>';
+    html += '</tr>';
+
+    return html;
+}
+
+
+function renderCoffees(coffees) {
+    var html = '';
+    for(var i = 0; i < coffees.length; i++) {
+        html += renderCoffee(coffees[i]);
+    }
+    return html;
+}
+
+
+//+++++++++++++++++++++++++++++++++++++++++---TOP---++++++++++++++++++++++++++++++++++++++
+
+//empty array that collects
+let filteredCoffees = [];
+
+//coffee container to display results
 let coffeeContainer = document.getElementById('coffee-list');
-let searchSubmit = document.getElementById("search-btn");
-let searchCoffee = document.getElementById("coffee-search");
-let roast = document.getElementById("roast-selection"); // our select tag
-searchSubmit.addEventListener("click", updateCoffees);
+
+
+//Select-options for Light/Mid/Large
+let roast = document.getElementById("roast-selection");
+//Event Listener for changes when switching Light/Mid/Large
 roast.addEventListener('change', updateCoffees);
 
-console.log('coffeeContainer: ', coffeeContainer);
-console.log('searchSubmit: ', searchSubmit);
-console.log('searchCoffee: ', searchCoffee);
-console.log('roast: ', roast);
+
+//Button #1
+let searchSubmit = document.getElementById("search-btn");
+//Event Listener for clicking submit for text-input.
+searchSubmit.addEventListener("click", updateCoffees);
+
+//+++++++++++++++++++++++++++++++++++++++---Bottom---+++++++++++++++++++++++++++++++++++++++
 
 
+//Button #2
+let searchCoffee = document.getElementById("coffee-search");
 
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
+
+let evt = "";
+const input = document.querySelector('input');
+
+
+//Object-Array of coffee
+let coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
     {id: 3, name: 'Cinnamon', roast: 'light'},
@@ -33,58 +73,44 @@ var coffees = [
 ];
 
 
-function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td class="coffee-name">' + coffee.name + '</td>';
-    html += '<td class="roast">' + coffee.roast + '</td>';
-    html += '</tr>';
 
-    return html;
-}
 
-function renderCoffees(coffees) {
-    var html = '';
-    for(var i = 0; i < coffees.length; i++) {
-        html += renderCoffee(coffees[i]);
-    }
-    return html;
+
+//Logs users key strokes
+function updateValue(e) {
+    console.log(e.target.value)
+    // return e.target.value;
 }
 
 
-// +++++++++++++++++
 function updateCoffees(e) {
-    // console.log('updateCoffees(): ');
-    
+    //it prevents the event from extending past the parameter of which it is assigned
     e.preventDefault(); // don't submit the form, we just want to update the data
-    let filteredCoffees = [];
+    //stores the lowercase value of the light/mid/dark
     let selectedRoast = roast.value.toLowerCase()
-    for (let i = 0; i < coffees.length;i++)
+    for (let i = 0; i < coffees.length; i++)
     {
-        console.log('coffee roast', coffees[i].roast);
-        console.log(selectedRoast);
+        //if roast type === roast value L/M/D
         if (coffees[i].roast == selectedRoast) {
+            //if light/mid/dark then "push" coffee with that value
             filteredCoffees.push(coffees[i]);
         }
+        ///if roast type === roast value L/M/D
         if(selectedRoast === "all"){
             filteredCoffees.push(coffees[i]);
         }
     }
-    // console.log("filteredCoffees: ",filteredCoffees);
-    // console.log('roast.value', roast.value);
-    // console.log("coffees: ", coffees);
+
     
     coffeeContainer.innerHTML = renderCoffees(filteredCoffees);
-
+    return filteredCoffees;
 
 }
 
-const input = document.querySelector('input');
-const log = document.getElementById('coffeeChoice');
 
-input.addEventListener('input', updateValue);
 
-function updateValue(e) {
-  console.log(e.target.value);
-}
+
+
+
+
 
