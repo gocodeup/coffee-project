736eadd79a4be6570744e6c2bd8df80b897981e8
paulswann44 <99698080+paulswann44@users.noreply.github.com>
2022-11-13 13:49:23 -0600

    project updated-HTML/CSS/JS/IMG

--- main.js
+++ main.js
@@ -23,75 +23,119 @@ function renderCoffee(coffee) {
     html += '<h1>' + coffee.name + '</h1>';
     html += '<p class="ms-3 roast-type">' + coffee.roast + '</p>';
     html += '</div>'
-    return html; //it will return: <div class=coffee><h1>coffee.name</h1><p class= "ms-3">coffee.roast</p></div>
+    return html;
 }
+//Notes:
+//it will return something like this: <div class=coffee><h1>coffee.name</h1><p class= "ms-3">coffee.roast</p></div>
+
 
 // This function will loop through renderCoffee, in the event there is an array
 //Any array has to be reversed later
 function renderCoffees(coffees) {
-    let html = ''; //string Bucket
-    for(let i = coffees.length - 1; i >= 0; i--) { // <= the order of [i] will display highest ID # to lowest ID #
-        html += renderCoffee(coffees[i]); //html = "renderedCoffee(input[i])"
+    let html = '';
+    for(let i = coffees.length - 1; i >= 0; i--) {
+        html += renderCoffee(coffees[i]);
     }
-    return html; //it will return something like this: <div><h1>"NAME"</h1><p>"ROAST"</p></div>
+    return html;
 }
 
 
-//Filters #1: only by Selection Option #1
-//Needs another Filter for Text-Input #1
+
+
+//Filters Hybrid: Selection Option #1
 function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    let selectedRoast = roast.value;//roast selection value L/M/D, not "all"
-    let filteredCoffees = []; //array bucket
-    coffees.forEach(function(coffee) { //individualises to coffee
-        if (coffee.roast === selectedRoast) {//from the coffee-obj-arr roast L/M/D=== value of L/M/D, All is not included
+    e.preventDefault();
+    let selectedRoast = roastSelection.value;
+    let searchTerm = searchCoffee.value.toLowerCase();
+    let filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast && coffee.name.toLowerCase().includes(searchTerm)){
             filteredCoffees.push(coffee);
         }
     });
-    coffeeDiv.innerHTML = renderCoffees(filteredCoffees); //write in the inner coffeeDiv in the format presented in the function rendersCoffee and looped via renderedCoffeeS
+    coffeeDiv.innerHTML = renderCoffees(filteredCoffees);
 
     if(selectedRoast === 'all') {
         coffeeDiv.innerHTML = renderCoffees(coffees);
-    }
-}
-
-//Filters #2: only by text-input #1
-function coffeeSearch(e) {
-    e.preventDefault();
-    let searchTerm = searchCoffee.value.toLowerCase(); //converts input text to lowercase
-    let filteredCoffees = [];//array bucket to collect stuff
-    coffees.forEach(function(coffee){//looping the var of coffees
-        if (coffee.name.toLowerCase().includes(searchTerm)){ //Thank Gil for this tip
-            filteredCoffees.push(coffee);
         }
-    })
-    coffeeDiv.innerHTML = renderCoffees(filteredCoffees);
-    //write w/in coffeeDiv in the format displayed in the function renderCoffee the filteredCoffees
 }
+//Notes:
+// This is a hybrid of both filter #1 & filter #2.
+
+
+// //Filters #1: only by Selection Option #1
+// function updateCoffees(e) {
+//     e.preventDefault();
+//     let selectedRoast = roast.value;
+//     let filteredCoffees = [];
+//     coffees.forEach(function(coffee) { //individualises to coffee
+//         if (coffee.roast === selectedRoast) {
+//             filteredCoffees.push(coffee);
+//         }
+//     });
+//     coffeeDiv.innerHTML = renderCoffees(filteredCoffees);
+//
+//     if(selectedRoast === 'all') {
+//         coffeeDiv.innerHTML = renderCoffees(coffees);
+//     }
+// }
+
+
+
+
+// //Filters #2: only by text-input #1
+// function coffeeSearch(e) {
+//     e.preventDefault();
+//     let searchTerm = searchCoffee.value.toLowerCase();
+//     let filteredCoffees = [];
+//     coffees.forEach(function(coffee){
+//         if (coffee.name.toLowerCase().includes(searchTerm)){
+//             filteredCoffees.push(coffee);
+//         }
+//     })
+//     coffeeDiv.innerHTML = renderCoffees(filteredCoffees);
+// }
 
 // +++++++++++++++++++++++++++++++ADD COFFEE SECTION UNFINISHED++++++++++++++++++++++++++++++++
 
-
+//Hypothesis #1
 function addCoffeeItem(e) {
     e.preventDefault();
-    let newCoffeeArr = []; //bucket for new coffee items arraY
-    let newCoffeeObj = { //bucket for new obj
-        id: coffees.length + 1, //the length + 1... so this should compute to be i = 15, especially since we are pushing
-        name: coffeeQuery.value, //#query-coffee's value
-        roast: roastAdd.value //added the roast's value
-    };//empty object to collect variables
-    newCoffeeArr.push(newCoffeeObj); //pushing the newCoffeeObj within the empty newCoffee Array
-    coffeeDiv.innerHTML = renderCoffee(newCoffeeArr);
+    let newCoffeeObj = {
+        id: coffees.length + 1,
+        name: addCoffeeName.value,
+        roast: roastAdd.value
+    };
+    coffees.push(newCoffeeObj);
+    coffeeDiv.innerHTML = renderCoffee(coffees);
+    console.log(coffees)
 }
-// coffeeDiv.innerHTML = renderCoffee(newCoffee);
-//https://stackoverflow.com/questions/40250139/how-can-i-push-an-object-into-an-array
+
+// Notes:
+//It works but anything gets pushed into var= coffees.
+//It will push
+
+
+//Hypothesis #2
+// function addCoffeeItem(e) {
+//     e.preventDefault();
+//     let id = coffees.length + 1;
+//     let name =  coffeeQuery.value;
+//     let roast =  roastAdd.value;
+//     coffees.push({id, name, roast}); //pushing the newCoffeeObj within the empty newCoffee Array
+//     console.log(coffees);
+//     coffeeDiv.innerHTML = renderCoffee(coffees);
+// }
+
+//NOTES:
+//Hypothesis #2 does not work properly.  It does not recognize the name and roast variables.
 
 
 
 
 //Option Selector #1
-let roast = document.querySelector('#roast-selection'); //given
-roast.addEventListener("change",updateCoffees); //given, but modified to change
+let roastSelection = document.querySelector('#roast-selection'); //given
+roastSelection.addEventListener("change",updateCoffees); //given, but modified to change
 
 
 //Button #1
@@ -101,33 +145,35 @@ submitButton.addEventListener('click', updateCoffees); //given
 
 //text input #1
 let searchCoffee= document.querySelector('#coffee-search'); //given
-searchCoffee.addEventListener('input', coffeeSearch); //created
+searchCoffee.addEventListener('keyup', updateCoffees); //created
 
-//I hate the naming convetion used!!!!!!!!!!!!!!!!!!!!!
-//linked to the function renderCoffeeS(the for loop), NOT renderCoffee w/o "s" (the format)
-//EXPLAINATION:
-// write w/in .coffeeDiv in the format provided by the function renderCoffee, the coffees object-array,
-// but in a reverse array order
-coffeeDiv.innerHTML = renderCoffees(coffees.reverse()); //modified from tbody.innerHTML
+// Notes:
+//Keydown & Keypress do not work for the addEventListener
+//The addEventListener is linked to the hybrid filter function, but if one is wanting to use both independent filter function:
+// (1) comment out the hybrid function and reinstate both filter #1 (updateCoffees) & filter #2 (coffeeSearch) functions
+// (2) for "searchCoffee.addEventListener", switch function to coffeeSearch.
+
+coffeeDiv.innerHTML = renderCoffees(coffees); //given, modified tbody.innerHTML to coffeeDiv
 
 
 
 
-// ++++++++++++++++++++++++++++++++++++++ADD COFFEE+++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //Selection Option #2
 // it is click b/c we are submitting the request to add to "Coffees" - object array
 let roastAdd = document.querySelector('#roast-add');
-// roast.addEventListener("click",);
+roastAdd.addEventListener("click", addCoffeeItem);
 
 
 //Button #2
-let buttonSubmit = document.querySelector('#submit-button');
-// buttonSubmit.addEventListener('click', updateCoffees);
+let addButton = document.querySelector('#add-button');
+addButton.addEventListener('click', addCoffeeItem);
 
 //text input #2
-let coffeeQuery= document.querySelector('#coffee-query');
-// coffeeQuery.addEventListener('input',addCoffeeItem );
+let addCoffeeName= document.querySelector('#coffee-query');
+addCoffeeName.addEventListener('keyup',addCoffeeItem);
+
 
 
 
