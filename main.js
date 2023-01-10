727904e0cbbbecf0b6d3d810e5c5b209ed4636e8
Robert Arroyos <robertarroyosjr@gmail.com>
2023-01-10 15:55:50 -0600

    added all filter for roastSelection

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function updateCoffees(e) {
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
     coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
+        if (coffee.roast === selectedRoast || coffee.group === selectedRoast) {
             filteredCoffees.push(coffee);
         }
     });
@@ -32,20 +32,20 @@ function updateCoffees(e) {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light', group: 'all'},
+    {id: 2, name: 'Half City', roast: 'light', group: 'all'},
+    {id: 3, name: 'Cinnamon', roast: 'light', group: 'all'},
+    {id: 4, name: 'City', roast: 'medium', group: 'all'},
+    {id: 5, name: 'American', roast: 'medium', group: 'all'},
+    {id: 6, name: 'Breakfast', roast: 'medium', group: 'all'},
+    {id: 7, name: 'High', roast: 'dark', group: 'all'},
+    {id: 8, name: 'Continental', roast: 'dark', group: 'all'},
+    {id: 9, name: 'New Orleans', roast: 'dark', group: 'all'},
+    {id: 10, name: 'European', roast: 'dark', group: 'all'},
+    {id: 11, name: 'Espresso', roast: 'dark', group: 'all'},
+    {id: 12, name: 'Viennese', roast: 'dark', group: 'all'},
+    {id: 13, name: 'Italian', roast: 'dark', group: 'all'},
+    {id: 14, name: 'French', roast: 'dark', group: 'all'},
 ];
 
 var tbody = document.querySelector('#coffees');
