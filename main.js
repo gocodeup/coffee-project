d6fefcf30f31607edc7df9830b758d027e2ddf8a
saidshah <saidmshah93@gmail.com>
2023-01-11 16:35:53 -0600

    single function initiated

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    // html += '<td>' + coffee.id + '</td>';
-       html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
-
-
+    var html = '<div class="coffee col-6 coffee d-flex">';
+    html += '<h5 class="mx-2">' + coffee.name +'</h5>';
+    html += '<p class="text-secondary">' + coffee.roast + '</p>'; 
+    html += '</div>';
     return html;
 }
 function renderCoffees(coffees) {
@@ -18,75 +15,33 @@ function renderCoffees(coffees) {
     return html;
 }
 
-function renderSearchedCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    // html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
-
-    return html;
-}
-function renderSearchedCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
-        html += renderSearchedCoffee(coffees[i]);
-    }
-    return html;
-}
-
-
-// ##### the function that works on keyups ######
-function updateCoffeesOnType(e){
-    e.preventDefault();
-    var selectedRoastForSearch = roastSelection.value;
-    var selectCoffeeOnSearch = coffeeSearch.value.toLowerCase();
-    console.log(selectCoffeeOnSearch)
-    var searchedCoffee = [];
-
-    if(selectedRoastForSearch === 'all' & selectCoffeeOnSearch === ''){
-        coffees.forEach(function(coffee){
-            searchedCoffee.push(coffee)
-        });
-    }else if(selectedRoastForSearch === 'all' & selectCoffeeOnSearch !== ''){
-        coffees.forEach(function(coffee){
-            if(coffee.name.toLowerCase().includes(selectCoffeeOnSearch)){
-                searchedCoffee.push(coffee)
-            }
-        });
-    }
-
-    if(selectCoffeeOnSearch === ''){
-        coffees.forEach(function(coffee){
-            searchedCoffee.push(coffee)
-        });
-    }else if(selectCoffeeOnSearch !== ""){
-        coffees.forEach(function(coffee){
-            if(coffee.name.includes(selectCoffeeOnSearch) & coffee.roast === selectedRoastForSearch){
-                searchedCoffee.push(coffee)
-            }
-        });
-    }
-    tbody.innerHTML = renderSearchedCoffees(searchedCoffee);
-}
-
-
-
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
+    var selectCoffeeOnSearch = coffeeSearch.value.toLowerCase();
     var filteredCoffees = [];
-    if(selectedRoast === 'all'){   //if the roast matches with value of selectedRoast.value(all)
-        coffees.forEach(function(coffee){
-            filteredCoffees.push(coffee);
-        });   
-     }else if(selectedRoast !== 'all'){
+    if(selectedRoast === 'all' & selectCoffeeOnSearch === ''){
         coffees.forEach(function(coffee) {
-            if (coffee.roast === selectedRoast) {
-                filteredCoffees.push(coffee);
+                filteredCoffees.push(coffee);   
+         });
+    }else if(selectedRoast === 'all' & selectCoffeeOnSearch !== ''){
+        coffees.forEach(function(coffee){
+            if(coffee.name.toLowerCase().includes(selectCoffeeOnSearch)){
+                filteredCoffees.push(coffee)
             }
         });
+    }else if(selectedRoast !== 'all'){
+        coffees.forEach(function(coffee) {
+            if (coffee.roast === selectedRoast & selectCoffeeOnSearch === '') {
+                filteredCoffees.push(coffee);
+            }else if(coffee.roast === selectedRoast & selectCoffeeOnSearch !== ''){
+                if(coffee.name.toLowerCase().includes(selectCoffeeOnSearch)){
+                    filteredCoffees.push(coffee)
+                }
+            }     
+        });
     }
+    
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
@@ -111,12 +66,9 @@ var coffees = [
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
-var roastOnSelection = document.querySelector('#roast-selection');
 var coffeeSearch = document.querySelector('#search-coffee');
-
-// tbody.style.display="none";
 tbody.innerHTML = renderCoffees(coffees);
 // Event listeners
 submitButton.addEventListener('click', updateCoffees);
-roastOnSelection.addEventListener('change', updateCoffees);
-coffeeSearch.addEventListener('keyup', updateCoffeesOnType);
+roastSelection.addEventListener('change', updateCoffees);
+coffeeSearch.addEventListener('keyup', updateCoffees);
