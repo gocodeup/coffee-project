ea17476e5ac69fe53ed7c4a5dcd5cab638bf08fb
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-03-15 15:47:47 -0500

    updated branch

--- main.js
+++ main.js
@@ -20,13 +20,30 @@ function renderCoffees(coffees) {
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     selectedRoast = selectedRoast.value;
+    emptyBox = emptyBox.value;
+    console.log(emptyBox);
     let filteredCoffees = [];
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        }
-    });
-    coffeeList.innerHTML = renderCoffees(filteredCoffees);
+    if (emptyBox === ``) {
+        coffees.forEach(function (coffee) {
+            if (coffee.roast === selectedRoast) {
+                filteredCoffees.push(coffee);
+            }
+            if (selectedRoast === 'all') {
+                filteredCoffees.push(coffee);
+            }
+        });
+    }
+    if (emptyBox !== ``) {
+        coffees.forEach(function (coffee) {
+            if (coffee.roast === emptyBox && coffee.name.toLowerCase().includes(emptyBox.toLowerCase())) {
+                filteredCoffees.push(coffee);
+            }
+            if (selectedRoast === 'all' && coffee.name.toLowerCase().includes(emptyBox.toLowerCase())) {
+                filteredCoffees.push(coffee);
+            }
+        });
+        tbody.innerHTML = renderCoffees(filteredCoffees);
+    }
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
@@ -47,9 +64,12 @@ let coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
+let tbody = document.querySelector('#coffee-body');
 let coffeeList = document.querySelector('#coffee-list');
 let topSubmitBtn = document.querySelector('#top-submit-btn');
 let selectedRoast = document.querySelector('#roast1');
+let emptyBox = document.querySelector('#coffee-name');
 
 coffeeList.innerHTML = renderCoffees(coffees);
 selectedRoast.addEventListener("change", updateCoffees);
+emptyBox.addEventListener('keyup', updateCoffees);
