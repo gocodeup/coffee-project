da0348076a3a2fa2bc5120f26a4fabc5f9ccfd77
Cole Usher <coleausher97@gmail.com>
2023-03-22 14:32:44 -0500

    Coffee Project Update

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+    var html = '<div class="coffee">';
+    html += '<div>' + coffee.id + '</div>';
+    html += '<div><h2>' + coffee.name + '</div></h2>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
 
     return html;
 }
