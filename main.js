c4cf83586eaffe925d0608658537e9810eeb4245
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-03-23 12:50:06 -0500

    Fixed

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+    let html = '<div class="coffee">';
+    // html += '<div>' + coffee.id + '</div>';
+    html += '<div><h2>' + coffee.name + '</div></h2>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
 
     return html;
 }
 
 function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    let html = '';
+    for(let i = coffees.length - 1; i >= 0; i--) {
         html += renderCoffee(coffees[i]);
     }
     return html;
@@ -20,18 +20,45 @@ function renderCoffees(coffees) {
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
+    let selectedRoast = roastSelection.value;
+    let filteredCoffees = [];
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
+// 3rd TODO: instead of rendering all of the coffees we need to grab the value from the
+// search bar and "filter" the coffee that we render
+// const searchBar = document.querySelector('#search')
+// const searchTerm = searchBar.value;
+
+//No filter the coffees based off of the value
+// Use this https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+// just copy the example and paste below
+// so use the include methond?
+// const result = coffees.filter(singleCoffee => singleCoffee.value.includes());// Im looking at examples right now ;If I was a junior at your level
+// I would look up "How to check if string is inside of string js"
+//                                              ^ Now write and expression that returns true if that coffee
+// console.log(result);
+
+let userInput = document.getElementById("search");
+
+userInput.addEventListener('keyup', function () {
+    let searchBoxInput = userInput.value;
+    let filteredCoffees = [];
+    for(let i = 0; i < coffees.length; i++){
+        if(coffees[i].name.includes(searchBoxInput))
+            filteredCoffees.push(coffees[i])
+    }
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+})
+//                   ^ We are rendering coffee here
+
+
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
+let coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
     {id: 3, name: 'Cinnamon', roast: 'light'},
@@ -48,10 +75,19 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
-var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+
+// 1st TODO: You need to make a querySelector for getting the search bar.
+
+let tbody = document.querySelector('#coffees');
+let submitButton = document.querySelector('#submit');
+let roastSelection = document.querySelector('#roast-selection');
 
 tbody.innerHTML = renderCoffees(coffees);
 
+
+// 2nd TODO: You need to create and event listener that listens for when
+// --> the user has put something in the search bar.
+roastSelection.addEventListener('input', updateCoffees)
+searchBar.addEventListener('input', updateCoffees)
 submitButton.addEventListener('click', updateCoffees);
+
