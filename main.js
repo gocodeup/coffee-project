843bd537fd7e4170bcc9e5b1bb74ae6d7c69bceb
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-04-18 10:34:02 -0500

    fixed case sensitive roast selection

--- main.js
+++ main.js
@@ -28,10 +28,10 @@ function updateCoffees(e) {
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
     coffees.forEach(function (coffee) {
-        if (coffee.roast === selectedRoast) {
+        if (coffee.roast.toUpperCase() === selectedRoast.toUpperCase()) {
             filteredCoffees.push(coffee);
             tbody.innerHTML = renderCoffees(filteredCoffees);
-        } else if (selectedRoast === 'all') {
+        } else if (selectedRoast === 'All') {
             tbody.innerHTML = renderCoffees(coffees);
         }
     })
