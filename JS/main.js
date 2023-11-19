bec7f8166fe2db019567ee453ea1fcff8b377194
Severiano Bautista <severiano.bautista090@gmail.com>
2023-11-19 17:31:23 -0600

    new coffee ids, hid ids added gif

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,13 @@
 "use strict"
+// SEV: ADDED A COUNTER TO ALLOW INCREMENTING OF ID
+let counter = 15;
 
 function renderCoffee(coffee) {
     let html = '<tr class="coffee">';
-    html += `<td>${coffee.id}</td>`;
+    // SEV: ADDED COUNTER TO INCREMENT ON NEW COFFEES, OR TO CALL EXISTING ID'S ON CURRENT COFFEES
+    html += `<td>${coffee.id || counter++}</td>`;
+    // html += `<td>${counter++}</td>`;
+    // html += `<td>${coffee.id}</td>`;
     html += `<td>${coffee.name}</td>`;
     html += `<td>${coffee.roast}</td>`;
     html += '</tr>';
@@ -12,22 +17,41 @@ function renderCoffee(coffee) {
 
 function renderCoffees(coffees) {
     let html = '';
-    for(let i = coffees.length - 1; i >= 0; i--) {
+    for (let i = coffees.length - 1; i >= 0; i--) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
 
+// function updateCoffees(e) {
+//     e.preventDefault(); // don't submit the form, we just want to update the data
+//     const selectedRoast = roastSelection.value;
+//     const filteredCoffees = [];
+//     coffees.forEach( coffee => {
+//         if (coffee.roast === selectedRoast) {
+//             filteredCoffees.push(coffee);
+//         }
+//     });
+//     tbody.innerHTML = renderCoffees(filteredCoffees);
+// }
+
+// SEV: CHECKING IF AN OPTION IS ALL ROASTS, IF SO WILL DISPLAY ALL ROASTS
+// ADDED AN IF STATEMENT FOR CHECKING FOR ALL ROASTS FIRST THEN CONTINUE WITH THE FUNCTION
+// NOT SURE IF THERE'S A PRETTIER WAY TO SHOW THIS
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     const selectedRoast = roastSelection.value;
-    const filteredCoffees = [];
-    coffees.forEach( coffee => {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        }
-    });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+    if (selectedRoast === "All Roasts") {
+        tbody.innerHTML = renderCoffees(coffees);
+    } else {
+        const filteredCoffees = [];
+        coffees.forEach(coffee => {
+            if (coffee.roast === selectedRoast) {
+                filteredCoffees.push(coffee);
+                tbody.innerHTML = renderCoffees(filteredCoffees);
+            }
+        })
+    }
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
@@ -48,11 +72,12 @@ const coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
+// SEV: SEARCH BAR CODE TO MAKE IT CASE INSENSITIVE WHEN LOOKING FOR A PARTICULAR BREW
 
 const searchBar = document.getElementById("coffee-name");
 
 searchBar.addEventListener("input", function () {
-    const searchTerm = this.value.toLowerCase(); // Convert the search term to lowercase for case-insensitive comparison
+    const searchTerm = this.value.toLowerCase();
     const resultsList = coffees.filter(coffee => coffee.name.toLowerCase().includes(searchTerm));
 
     tbody.innerHTML = renderCoffees(resultsList);
@@ -79,9 +104,9 @@ function addNewCoffee() {
     const idCell = document.createElement('td');
     const nameCell = document.createElement('td');
     const roastCell = document.createElement('td');
-
+    // SEV: COUNTER FOR ID'S, GENERATES DYNAMICALLY
     // Set values for the cells
-    idCell.textContent = "";  // You may generate a unique ID dynamically
+    idCell.textContent = counter++;
     nameCell.textContent = newCoffeeName;
     roastCell.textContent = newCoffeeRoast;
 
@@ -106,4 +131,5 @@ const roastSelection = document.querySelector('#roast-selection');
 tbody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
+
 
