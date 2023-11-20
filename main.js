4b959a72dbda0f27215cedd16f25f5d743247a6f
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2023-11-19 23:09:50 -0600

    updated coffee project

--- main.js
+++ main.js
@@ -1,81 +1,55 @@
 "use strict"
 
 // ALL I DID TO THIS FUNCTION WAS CHANGE THE tr TO A div AND THE td's TO p TAGS.
+// Given functions
+
 function renderCoffee(coffee) {
     let html = '<div class="coffee">';
-    html += `<p>${coffee.id}</p>`;
-    html += `<p>${coffee.name}</p>`;
+    html += `<h5>${coffee.name}</h5>`;
     html += `<p>${coffee.roast}</p>`;
     html += '</div>';
-
     return html;
 }
 
 function renderCoffees(coffees) {
     let html = '';
-    for(let i = coffees.length - 1; i >= 0; i--) {
+    for (let i = coffees.length - 1; i >= 0; i--) {
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
 
-// THE ONLY MODIFICATION TO THIS FUNCTION WAS THE VARIABLE NAME
-// ON LINE 33 (CHANGED FROM tbody TO coffeesList)
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
+// Filter function with slight adjustments
+
+function updateCoffees() {
     const selectedRoast = roastSelection.value.toLowerCase();
     const searchTerm = userTexts.value.trim().toLowerCase();
-    console.log('Selected Roast:', selectedRoast);
-    console.log('Search Term:', searchTerm);
 
-    const filteredCoffees = [];
-    coffees.forEach( coffee => {
-        console.log('Coffee Roast:', coffee.roast.toLowerCase());
-        console.log('Coffee Name:', coffee.name.toLowerCase());
-
-        if (coffee.roast.toLowerCase() === selectedRoast){
-            const matchesRoast = coffee.roast.toLowerCase() === selectedRoast || selectedRoast === 'all';
-            const matchesSearch = coffee.name.toLowerCase().includes(searchTerm);
-            console.log('Matches Roast:', matchesRoast);
-            console.log('Matches Search:', matchesSearch);
-
-            // filteredCoffees.push(coffee); /*without this in the code it doesn't populate */
-            if (matchesRoast && matchesSearch);
-            filteredCoffees.push(coffee); /*without this in the code it doesn't populate */
-        }
+    const filteredCoffees = coffees.filter(coffee => {
+        const matchesRoast = coffee.roast.toLowerCase() === selectedRoast || selectedRoast === 'all';
+        const matchesSearch = coffee.name.toLowerCase().includes(searchTerm);
+        return matchesRoast && matchesSearch;
     });
-    console.log('Filtered Coffees:', filteredCoffees);
 
     coffeesList.innerHTML = renderCoffees(filteredCoffees);
 }
 
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+// The provided coffee data
 const coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    { id: 1, name: 'Light City', roast: 'light' },
+    // ... (other coffee objects)
+    { id: 14, name: 'French', roast: 'dark' },
 ];
 
-// ON LINE 55 CHANGED VARIABLE NAME FROM tbody TO coffeesList;
+// Variable targeting the #coffees div
 const coffeesList = document.querySelector('#coffees');
-// (ON LINE 57) ALSO TESTED IT BY TARGETING THE DIV USING getElementById // THIS ALSO WORKS
-// const coffeesList = document.getElementById('coffees');
-const submitButton = document.querySelector('#submit');
 const roastSelection = document.querySelector('#roast-selection');
-const userTexts = document.querySelector('#userText')
+const userTexts = document.querySelector('#userText');
 
-// ON LINE 62 CHANGED VARIABLE NAME FROM tbody TO coffeesList;
+// Initial rendering of all coffees
 coffeesList.innerHTML = renderCoffees(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
+// Event listeners for filtering coffees
+
+userTexts.addEventListener('input', updateCoffees); // Real-time filtering while typing
+roastSelection.addEventListener('change', updateCoffees); // On roast selection change
