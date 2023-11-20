4f211e830ceee4330ed818766b0d82f91771517f
Severiano Bautista <severiano.bautista090@gmail.com>
2023-11-20 13:54:36 -0600

    updating changes

--- JS/main.js
+++ JS/main.js
@@ -122,3 +122,6 @@ const roastSelection = document.querySelector('#roast-selection');
 section.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
+
+//============================================================//
+
