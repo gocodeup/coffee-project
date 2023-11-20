b0fe3c597d07439687a49b97fa3d17fb3933e45d
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-11-20 10:01:06 -0600

    updated coffee project

--- main.js
+++ main.js
@@ -22,10 +22,40 @@ function updateCoffees(e) {
     e.preventDefault();
     const selectedRoast = roastSelection.value;
     const searchTerm = coffeeSearch.value.toLowerCase();
-    const filteredCoffees = coffees.filter(coffee =>
-        coffee.roast === selectedRoast || coffee.name.includes(searchTerm)
-    );
+
+    const filteredCoffees = [];
+    coffees.forEach(coffee => {
+        const matchRoast = selectedRoast === 'all' || coffee.roast === selectedRoast;
+        const matchName = coffee.name.toLowerCase().includes(searchTerm);
+        if (matchRoast && matchName) {
+            filteredCoffees.push(coffee);
+        }
+    });
+
     tbody.innerHTML = renderCoffees(filteredCoffees);
+}
+
+function addCoffees(e) {
+    e.preventDefault();
+    const addedCoffee = addCoffee.value;
+    const addedRoast = addRoast.value;
+    const addedID = coffees.length + 1;
+
+    //make new object
+    const newCoffee = {
+        id: addedID,
+        name: addedCoffee,
+        roast: addedRoast
+    };
+    //add object to array
+    coffees.push(newCoffee)
+
+    // Clear the input fields
+    addCoffee.value = '';
+    addRoast.value = '';
+
+    // Render the updated list
+    tbody.innerHTML = renderCoffees(coffees);
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
@@ -50,9 +80,12 @@ const tbody = document.querySelector('#coffees');
 const coffeeForm = document.querySelector('#coffee-form');
 const roastSelection = document.querySelector('#roast-selection');
 const coffeeSearch = document.querySelector('#coffee-search');
-
+const addCoffee = document.querySelector('#add-coffee-search');
+const addRoast = document.querySelector('#add-roast-selection');
+const coffeeButton = document.querySelector('#add-coffee-btn')
 tbody.innerHTML = renderCoffees(coffees);
 
-coffeeForm.addEventListener('submit', updateCoffees);
+coffeeForm.addEventListener('change', updateCoffees);
 coffeeSearch.addEventListener('input', updateCoffees);
-
+roastSelection.addEventListener('change', updateCoffees);
+coffeeForm.addEventListener('submit', addCoffees)
