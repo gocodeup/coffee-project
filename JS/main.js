a268d99c1a96a7f06a948a68aca71888af293da7
Taylor Andersen <tay.andersen99@gmail.com>
2023-11-21 10:32:24 -0600

    Removed a line

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,6 @@
 
 function renderCoffee(coffee) {
     let html = '<div class="coffee">';
-    // html += `<div>${coffee.id}</div>`;
     html += `<h5 class="coffeeName">${coffee.name}</h5>`;
     html += `<p>${coffee.roast}</p>`;
     html += '</div>';
